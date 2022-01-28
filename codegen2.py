f50c9f99ddedbedabd483019ced8335d518dc824
KENZ <KENZ.gelsoft@gmail.com>
2022-01-29 08:32:09 +0900

    Move code parts into each file generator

--- codegen2.py
+++ codegen2.py
@@ -1,71 +1,6 @@
 import xml.etree.ElementTree as ET
 from doxybindgen import Class
 
-PROLOGUE = '''\
-#![allow(dead_code)]
-#![allow(non_upper_case_globals)]
-#![allow(unused_parens)]
-'''
-
-CXX_PROLOGUE = '''\
-use std::os::raw::c_char;
-use std::pin::Pin;
-
-use crate::macros::wx_class;
-
-// any pointer type used on ffi boundary.
-// we chose this type as it's handy in cxx.
-type UnsafeAnyPtr = *const c_char;
-
-#[cxx::bridge(namespace = "wxrust")]
-mod ffi {
-    #[namespace = ""]
-    unsafe extern "C++" {
-        include!("wx/include/wxrust.h");
-        include!("wx/include/wxrust2.h");
-'''
-CXX_PROLOGUE2 = '''\
-    }
-    unsafe extern "C++" {'''
-CXX_EPILOGUE = '''\
-    }
-}
-
-pub trait ObjectMethods {
-    unsafe fn as_ptr(&self) -> UnsafeAnyPtr;
-    fn pinned<T>(&self) -> Pin<&mut T> {
-        unsafe { Pin::new_unchecked(&mut *(self.as_ptr() as *mut _)) }
-    }
-}
-'''
-
-H_PROLOGUE = '''\
-#pragma once
-#include <wx/wx.h>
-
-#include "rust/cxx.h"
-#include "wx/src/generated.rs.h"
-
-
-namespace wxrust {
-'''
-H_EPILOGUE = '''\
-} // namespace wxrust
-'''
-
-CC_PROLOGUE = '''\
-#include "wx/include/wxrust.h"
-#include "wx/include/wxrust2.h"
-
-namespace wxrust {
-
-// Constructors
-'''
-
-CC_EPILOGUE = '''\
-} // namespace wxrust
-'''
-
 types = [
     'wxPoint',
     'wxSize',
@@ -186,37 +121,88 @@ def generate_bindings_for(classes):
                 print(chunk, file=f)
 
 def generated_rs(classes):
-    yield PROLOGUE
-    yield CXX_PROLOGUE
+    yield '''\
+#![allow(dead_code)]
+#![allow(non_upper_case_globals)]
+#![allow(unused_parens)]
+
+use std::os::raw::c_char;
+use std::pin::Pin;
+
+use crate::macros::wx_class;
+
+// any pointer type used on ffi boundary.
+// we chose this type as it's handy in cxx.
+type UnsafeAnyPtr = *const c_char;
+
+#[cxx::bridge(namespace = "wxrust")]
+mod ffi {
+    #[namespace = ""]
+    unsafe extern "C++" {
+        include!("wx/include/wxrust.h");
+        include!("wx/include/wxrust2.h");
+'''
     indent = ' ' * 4 * 2
     for t in types:
         yield '%stype %s;' % (indent,t)
     for cls in classes:
         for chunk in cls.ffi_methods(BLOCKLIST):
             yield chunk
-    yield CXX_PROLOGUE2
+    yield '''\
+    }
+    unsafe extern "C++" {'''
     for cls in classes:
         for chunk in cls.ffi_ctors():
             yield chunk
-    yield CXX_EPILOGUE
+    yield '''\
+    }
+}
+
+pub trait ObjectMethods {
+    unsafe fn as_ptr(&self) -> UnsafeAnyPtr;
+    fn pinned<T>(&self) -> Pin<&mut T> {
+        unsafe { Pin::new_unchecked(&mut *(self.as_ptr() as *mut _)) }
+    }
+}
+'''
 
     for cls in classes:
         for chunk in cls.safer_binding():
             yield chunk
 
 def wxrust2_h(classes):
-    yield H_PROLOGUE
+    yield '''\
+#pragma once
+#include <wx/wx.h>
+
+#include "rust/cxx.h"
+#include "wx/src/generated.rs.h"
+
+
+namespace wxrust {
+'''
     for cls in classes:
         for chunk in cls.ctors_for_h():
             yield chunk
-    yield H_EPILOGUE
+    yield '''\
+} // namespace wxrust
+'''
 
 def wxrust2_cc(classes):
-    yield CC_PROLOGUE
+    yield '''\
+#include "wx/include/wxrust.h"
+#include "wx/include/wxrust2.h"
+
+namespace wxrust {
+
+// Constructors
+'''
     for cls in classes:
         for chunk in cls.ctors_for_cc():
             yield chunk
-    yield CC_EPILOGUE
+    yield '''\
+} // namespace wxrust
+'''
 
 if __name__ == '__main__':
     main()
