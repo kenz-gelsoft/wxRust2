082905d900d4396d9719a721ea1df932496b7c63
KENZ <KENZ.gelsoft@gmail.com>
2022-03-28 06:34:09 +0900

    Add or copy base package required files

---
+++ base/src/lib.rs
@@ -0,0 +1,117 @@
+use std::convert::TryInto;
+use std::os::raw::{c_char, c_int, c_void};
+use std::ptr;
+
+mod macros;
+
+mod defs;
+pub use defs::*;
+mod manual;
+pub use manual::*;
+
+mod generated;
+pub use generated::*;
+
+mod ffi {
+    use std::os::raw::{c_char, c_int, c_uchar, c_void};
+    extern "C" {
+        pub fn wxObject_delete(self_: *mut c_void);
+
+        pub fn AppSetOnInit(
+            aFn: *mut c_void,
+            aParam: *mut c_void
+        );
+        pub fn wxEvtHandler_Bind(
+            self_: *mut c_void,
+            eventType: c_int,
+            aFn: *mut c_void,
+            aParam: *mut c_void
+        );
+
+        // String
+        pub fn wxString_new(psz: *const c_uchar, nLength: usize) -> *mut c_void;
+        pub fn wxString_UTF8Data(self_: *mut c_void) -> *mut c_uchar;
+        pub fn wxString_Len(self_: *mut c_void) -> usize;
+        
+        pub fn wxRustEntry(argc: *mut c_int, argv: *mut *mut c_char) -> c_int;
+    }
+}
+
+fn from_wx_string(s: *mut c_void) -> String {
+    unsafe {
+        let utf8data = ffi::wxString_UTF8Data(s);
+        let len = ffi::wxString_Len(s);
+        return String::from_raw_parts(utf8data, len, len);
+    }
+}
+pub unsafe fn wx_string_from(s: &str) -> *const c_void {
+    return ffi::wxString_new(s.as_ptr(), s.len())
+}
+
+// Rust closure to wx calablle function+param pair.
+unsafe fn to_wx_callable<F: Fn() + 'static>(closure: F) -> (*mut c_void, *mut c_void) {
+    unsafe fn trampoline<F: Fn() + 'static>(closure: *mut c_void) {
+        let closure = &*(closure as *const F);
+        closure();
+    }
+    // pass the pointer in the heap to avoid move.
+    let closure = Box::new(closure);
+    (
+        trampoline::<F> as _,
+        Box::into_raw(closure) as _
+    )
+}
+
+pub trait Bindable {
+    fn bind<F: Fn() + 'static>(&self, event_type: c_int, closure: F);
+}
+impl<T: EvtHandlerMethods> Bindable for T {
+    fn bind<F: Fn() + 'static>(&self, event_type: c_int, closure: F) {
+        unsafe {
+            let (f, param) = to_wx_callable(closure);
+            ffi::wxEvtHandler_Bind(self.as_ptr(), event_type, f, param);
+        }
+    }
+}
+
+// wxApp
+pub enum App {}
+impl App {
+    pub fn on_init<F: Fn() + 'static>(closure: F) {
+        unsafe {
+            let (f, param) = to_wx_callable(closure);
+            ffi::AppSetOnInit(f, param);
+        }
+    }
+    pub fn run<F: Fn() + 'static>(closure: F) {
+        Self::on_init(closure);
+        entry();
+    }
+}
+
+// wxDefaultPosition
+impl Default for Point {
+    fn default() -> Self { Point::new1(-1, -1) }
+}
+// wxDefaultSize
+impl Default for Size {
+    fn default() -> Self { Size::new1(-1, -1) }
+}
+// wxDefaultValidator
+impl Default for Validator {
+    fn default() -> Self { Validator::new() }
+}
+
+// wxEntry
+pub fn entry() {
+    let args: Vec<String> = std::env::args().collect();
+    let mut argv: Vec<*mut c_char> = Vec::with_capacity(args.len() + 1);
+    for arg in &args {
+        argv.push(arg.as_ptr() as *mut c_char);
+    }
+    argv.push(ptr::null_mut()); // Nul terminator.
+    let mut argc: c_int = args.len().try_into().unwrap();
+    unsafe {
+        ffi::wxRustEntry(&mut argc, argv.as_mut_ptr());
+    }
+}
---
+++ base/src/manual.rs
@@ -0,0 +1,51 @@
+#![allow(dead_code)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+
+use std::os::raw::c_long;
+
+// TODO auto generate
+pub const wxRUST_EVT_BUTTON: i32 = 0;
+
+pub const fn wxPG_IT_CHILDREN(A: c_long) -> c_long {
+    (A) << 16
+}
+
+// wxDeprecatedGUIConstants
+/*  Text font families */
+pub const wxDEFAULT: i32 = 70;
+pub const wxDECORATIVE: i32 = 71;
+pub const wxROMAN: i32 = 72;
+pub const wxSCRIPT: i32 = 73;
+pub const wxSWISS: i32 = 74;
+pub const wxMODERN: i32 = 75;
+pub const wxTELETYPE: i32 = 76;
+
+/*  Proportional or Fixed width fonts (not yet used) */
+pub const wxVARIABLE: i32 = 80;
+pub const wxFIXED: i32 = 81;
+
+pub const wxNORMAL: i32 = 90;
+pub const wxLIGHT: i32 = 91;
+pub const wxBOLD: i32 = 92;
+/*  Also wxNORMAL for normal (non-italic text) */
+pub const wxITALIC: i32 = 93;
+pub const wxSLANT: i32 = 94;
+
+/*  Pen styles */
+pub const wxSOLID: i32 = 100;
+pub const wxDOT: i32 = 101;
+pub const wxLONG_DASH: i32 = 102;
+pub const wxSHORT_DASH: i32 = 103;
+pub const wxDOT_DASH: i32 = 104;
+pub const wxUSER_DASH: i32 = 105;
+
+pub const wxTRANSPARENT: i32 = 106;
+
+/*  Brush & Pen Stippling. Note that a stippled pen cannot be dashed!! */
+/*  Note also that stippling a Pen IS meaningful, because a Line is */
+pub const wxSTIPPLE_MASK_OPAQUE: i32 = 107;
+pub const wxSTIPPLE_MASK: i32 = 108;
+/*  drawn with a Pen, and without any Brush -- and it can be stippled. */
+pub const wxSTIPPLE: i32 = 110;
