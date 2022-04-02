9d63caf023a0364de8a1dfc3423b6dae0f4dcc7c
KENZ <KENZ.gelsoft@gmail.com>
2022-04-02 09:49:43 +0900

    Minimum change to cargo build succed on Win32
    * with x64 debug dll

--- wxrust-config/src/lib.rs
+++ wxrust-config/src/lib.rs
@@ -1,12 +1,13 @@
+use std::env;
 use std::process::Command;
 
 pub fn wx_config_cflags(cc_build: &mut cc::Build) -> &mut cc::Build {
     // from `wx-config --cflags`
     let cflags = wx_config(&["--cflags"]);
     // ignore too many warnings with wx3.0
-    cc_build.flag("-Wno-deprecated-copy");
-    cc_build.flag("-Wno-ignored-qualifiers");
-    cc_build.flag("-Wno-unused-parameter");
+    cc_build.flag_if_supported("-Wno-deprecated-copy")
+            .flag_if_supported("-Wno-ignored-qualifiers")
+            .flag_if_supported("-Wno-unused-parameter");
     for arg in cflags.split_whitespace() {
         if arg.starts_with("-I") {
             cc_build.include(&arg[2..]);
@@ -18,6 +19,10 @@ pub fn wx_config_cflags(cc_build: &mut cc::Build) -> &mut cc::Build {
         } else {
             panic!("unsupported argument '{}'. please file a bug.", arg)
         }
+    }
+    // required to use DLLs
+    if cfg!(windows) {
+        cc_build.define("WXUSINGDLL", "");
     }
     cc_build
 }
@@ -44,10 +49,32 @@ pub fn print_wx_config_libs_for_cargo() {
     }
 }
 
-pub fn wx_config(args: &[&str]) -> String {
-    let output = Command::new("wx-config")
-        .args(args)
-        .output()
-        .expect("failed execute wx-config command.");
-    String::from_utf8_lossy(&output.stdout).to_string()
+fn wx_config(args: &[&str]) -> String {
+    if cfg!(windows) {
+        let wxwin = env::var("wxwin")
+            .expect("Set 'wxwin' environment variable to point the wxMSW binaries dir.");
+        if args.contains(&"--cflags") {
+            let includes = vec![
+                format!("-I{}\\include", wxwin),
+                // TODO: determine this name automatically
+                format!("-I{}\\lib\\vc14x_x64_dll\\mswud", wxwin),
+            ];
+            includes.join(" ")
+        } else {
+            let libs = vec![
+                // TODO: determine this name automatically
+                format!("-L{}\\lib\\vc14x_x64_dll", wxwin),
+                // TODO: determine libraries list automatically
+                "-lwxbase31ud".to_string(),
+                "-lwxmsw31ud_core".to_string(),
+            ];
+            libs.join(" ")
+        }
+    } else {
+        let output = Command::new("wx-config")
+            .args(args)
+            .output()
+            .expect("failed execute wx-config command.");
+        String::from_utf8_lossy(&output.stdout).to_string()
+    }
 }
