3e90c07ed3617e56f1787d171b30b946e259f681
KENZ <KENZ.gelsoft@gmail.com>
2022-04-04 06:57:30 +0900

    Fix compile error

--- example/src/main.rs
+++ example/src/main.rs
@@ -5,7 +5,7 @@ use wx;
 use wx::*;
 
 fn main() {
-    wx_base::App::run(|| {
+    wx_base::App::run(|_| {
         let frame = Frame::new1(Window::none(), wxID_ANY, "Hello, 世界", 
                 &Point::default(), &Size::default(),
                 wxDEFAULT_FRAME_STYLE, "");
@@ -15,7 +15,7 @@ fn main() {
         let i = 3;
         println!("i={}", i);
         let button_copy = button.clone();
-        button.bind(wxRUST_EVT_BUTTON, move || {
+        button.bind(wxRUST_EVT_BUTTON, move |_| {
             println!("i={}", i);
             button_copy.set_label("clicked");
             println!("s={}", button_copy.get_label())
