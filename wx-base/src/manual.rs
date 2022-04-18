ec1717d73df4a909da3c50157c21fbb68315bd01
KENZ <KENZ.gelsoft@gmail.com>
2022-04-19 07:06:39 +0900

    Remove wx-prefix in manually written constants

--- wx-base/src/manual.rs
+++ wx-base/src/manual.rs
@@ -6,47 +6,47 @@
 use std::os::raw::c_long;
 
 // TODO auto generate
-pub const wxRUST_EVT_BUTTON: i32 = 0;
-pub const wxRUST_EVT_MENU: i32 = 1;
+pub const RUST_EVT_BUTTON: i32 = 0;
+pub const RUST_EVT_MENU: i32 = 1;
 
-pub const fn wxPG_IT_CHILDREN(A: c_long) -> c_long {
+pub const fn PG_IT_CHILDREN(A: c_long) -> c_long {
     (A) << 16
 }
 
-// wxDeprecatedGUIConstants
+// DeprecatedGUIConstants
 /*  Text font families */
-pub const wxDEFAULT: i32 = 70;
-pub const wxDECORATIVE: i32 = 71;
-pub const wxROMAN: i32 = 72;
-pub const wxSCRIPT: i32 = 73;
-pub const wxSWISS: i32 = 74;
-pub const wxMODERN: i32 = 75;
-pub const wxTELETYPE: i32 = 76;
+pub const DEFAULT: i32 = 70;
+pub const DECORATIVE: i32 = 71;
+pub const ROMAN: i32 = 72;
+pub const SCRIPT: i32 = 73;
+pub const SWISS: i32 = 74;
+pub const MODERN: i32 = 75;
+pub const TELETYPE: i32 = 76;
 
 /*  Proportional or Fixed width fonts (not yet used) */
-pub const wxVARIABLE: i32 = 80;
-pub const wxFIXED: i32 = 81;
+pub const VARIABLE: i32 = 80;
+pub const FIXED: i32 = 81;
 
-pub const wxNORMAL: i32 = 90;
-pub const wxLIGHT: i32 = 91;
-pub const wxBOLD: i32 = 92;
-/*  Also wxNORMAL for normal (non-italic text) */
-pub const wxITALIC: i32 = 93;
-pub const wxSLANT: i32 = 94;
+pub const NORMAL: i32 = 90;
+pub const LIGHT: i32 = 91;
+pub const BOLD: i32 = 92;
+/*  Also NORMAL for normal (non-italic text) */
+pub const ITALIC: i32 = 93;
+pub const SLANT: i32 = 94;
 
 /*  Pen styles */
-pub const wxSOLID: i32 = 100;
-pub const wxDOT: i32 = 101;
-pub const wxLONG_DASH: i32 = 102;
-pub const wxSHORT_DASH: i32 = 103;
-pub const wxDOT_DASH: i32 = 104;
-pub const wxUSER_DASH: i32 = 105;
+pub const SOLID: i32 = 100;
+pub const DOT: i32 = 101;
+pub const LONG_DASH: i32 = 102;
+pub const SHORT_DASH: i32 = 103;
+pub const DOT_DASH: i32 = 104;
+pub const USER_DASH: i32 = 105;
 
-pub const wxTRANSPARENT: i32 = 106;
+pub const TRANSPARENT: i32 = 106;
 
 /*  Brush & Pen Stippling. Note that a stippled pen cannot be dashed!! */
 /*  Note also that stippling a Pen IS meaningful, because a Line is */
-pub const wxSTIPPLE_MASK_OPAQUE: i32 = 107;
-pub const wxSTIPPLE_MASK: i32 = 108;
+pub const STIPPLE_MASK_OPAQUE: i32 = 107;
+pub const STIPPLE_MASK: i32 = 108;
 /*  drawn with a Pen, and without any Brush -- and it can be stippled. */
-pub const wxSTIPPLE: i32 = 110;
+pub const STIPPLE: i32 = 110;
