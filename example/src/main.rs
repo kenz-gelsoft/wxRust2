9f415ee25f44b8c499b6f05138160f1e19fae7f2
KENZ <KENZ.gelsoft@gmail.com>
2022-04-20 07:05:46 +0900

    Use `wx::` for sample codes

--- example/src/main.rs
+++ example/src/main.rs
@@ -4,22 +4,22 @@ use wx;
 use wx::methods::*;
 
 fn main() {
-    wx_base::App::run(|_| {
-        let frame = wx::Frame::new(wx::Window::none(), wx_base::ID_ANY, "Hello, 世界", 
+    wx::App::run(|_| {
+        let frame = wx::Frame::new(wx::Window::none(), wx::ID_ANY, "Hello, 世界", 
                 &wx::Point::default(), &wx::Size::default(),
-                wx_base::DEFAULT_FRAME_STYLE, "");
-        let button = wx::Button::new(Some(&frame), wx_base::ID_ANY, "Greet",
+                wx::DEFAULT_FRAME_STYLE, "");
+        let button = wx::Button::new(Some(&frame), wx::ID_ANY, "Greet",
                 &wx::Point::default(), &wx::Size::default(), 0,
                 &wx::Validator::default(), "");
         let i = 3;
         println!("i={}", i);
         let button_copy = button.clone();
-        button.bind(wx_base::RUST_EVT_BUTTON, move |_: &wx::CommandEvent| {
+        button.bind(wx::RUST_EVT_BUTTON, move |_: &wx::CommandEvent| {
             println!("i={}", i);
             button_copy.set_label("clicked");
             println!("s={}", button_copy.get_label())
         });
-        frame.centre(wx_base::BOTH);
+        frame.centre(wx::BOTH);
         frame.show(true);
     });
 }
--- samples/minimal/src/main.rs
+++ samples/minimal/src/main.rs
@@ -4,14 +4,14 @@ use std::os::raw::{c_int, c_long};
 use wx;
 use wx::methods::*;
 
-const MINIMAL_QUIT: c_int  = wx_base::ID_EXIT;
-const MINIMAL_ABOUT: c_int = wx_base::ID_ABOUT;
+const MINIMAL_QUIT: c_int  = wx::ID_EXIT;
+const MINIMAL_ABOUT: c_int = wx::ID_ABOUT;
 
 fn main() {
-    wx_base::App::run(|_| {
+    wx::App::run(|_| {
         let frame = MyFrame::new("Minimal wxRust App");
         let frame_copy = frame.clone();
-        frame.base.bind(wx_base::RUST_EVT_MENU, move |event: &wx::CommandEvent| {
+        frame.base.bind(wx::RUST_EVT_MENU, move |event: &wx::CommandEvent| {
             frame_copy.handle_menu(event)
         });
         frame.base.show(true);
@@ -24,13 +24,13 @@ struct MyFrame {
 }
 impl MyFrame {
     fn new(title: &str) -> Self {
-        let frame = wx::Frame::new(wx::Window::none(), wx_base::ID_ANY, title, 
+        let frame = wx::Frame::new(wx::Window::none(), wx::ID_ANY, title, 
                 &wx::Point::default(), &wx::Size::default(),
-                wx_base::DEFAULT_FRAME_STYLE, "");
+                wx::DEFAULT_FRAME_STYLE, "");
         let file_menu = wx::Menu::new();
         let help_menu = wx::Menu::new();
-        help_menu.append_int_str(MINIMAL_ABOUT, "&About\tF1", "Show about dialog", wx_base::ITEM_NORMAL);
-        file_menu.append_int_str(MINIMAL_QUIT, "E&xit\tAlt-X", "Quit this program", wx_base::ITEM_NORMAL);
+        help_menu.append_int_str(MINIMAL_ABOUT, "&About\tF1", "Show about dialog", wx::ITEM_NORMAL);
+        file_menu.append_int_str(MINIMAL_QUIT, "E&xit\tAlt-X", "Quit this program", wx::ITEM_NORMAL);
 
         let menu_bar = wx::MenuBar::new(0);
         menu_bar.append(Some(&file_menu), "&File");
@@ -38,7 +38,7 @@ impl MyFrame {
 
         frame.set_menu_bar(Some(&menu_bar));
 
-        frame.create_status_bar(2, wx_base::STB_DEFAULT_STYLE, 0, "statusBar");
+        frame.create_status_bar(2, wx::STB_DEFAULT_STYLE, 0, "statusBar");
         frame.set_status_text("Welcome to wxRust!", 0);
         MyFrame {
             base: frame,
@@ -62,7 +62,7 @@ impl MyFrame {
         wx::message_box(
             "Message",
             "About wxRust minimal sample",
-            (wx_base::OK | wx_base::ICON_INFORMATION as c_long).try_into().unwrap(),
+            (wx::OK | wx::ICON_INFORMATION as c_long).try_into().unwrap(),
             Some(&self.base),
         )
     }
