c6f8b526ced08fb31b3a4853379994b80f80286e
KENZ <KENZ.gelsoft@gmail.com>
2022-05-15 14:15:45 +0900

    Make wrapsizer methods to instance methods

--- samples/wrapsizer/src/main.rs
+++ samples/wrapsizer/src/main.rs
@@ -67,7 +67,7 @@ impl WrapSizerFrame {
         // Some toolbars in a wrap sizer
         let sizer_top = wx::BoxSizer::new(wx::HORIZONTAL);
         sizer_top.add_window_int(
-            Some(&Self::make_tool_bar(&self.m_panel)),
+            Some(&self.make_tool_bar()),
             0,
             0,
             0,
@@ -75,7 +75,7 @@ impl WrapSizerFrame {
         );
         sizer_top.add_int_int(20, 1, 0, 0, 0, wx::Object::none());
         sizer_top.add_window_int(
-            Some(&Self::make_tool_bar(&self.m_panel)),
+            Some(&self.make_tool_bar()),
             0,
             0,
             0,
@@ -83,7 +83,7 @@ impl WrapSizerFrame {
         );
         sizer_top.add_int_int(20, 1, 0, 0, 0, wx::Object::none());
         sizer_top.add_window_int(
-            Some(&Self::make_tool_bar(&self.m_panel)),
+            Some(&self.make_tool_bar()),
             0,
             0,
             0,
@@ -174,27 +174,27 @@ impl WrapSizerFrame {
         self.base.close(false);
     }
 
-    fn add_tool_bar_button(tb: &wx::ToolBar, label: &str, artid: &str) {
+    fn add_tool_bar_button(&self, tb: &wx::ToolBar, label: &str, artid: &str) {
         let bm =
             wx::ArtProvider::get_bitmap(artid, "wxART_OTHER_C", &wx::Size::new_with_int(16, 16));
         tb.add_tool_int_str(wx::ID_ANY, label, &bm, "", wx::ITEM_NORMAL);
     }
 
-    fn make_tool_bar(panel: &wx::Panel) -> wx::ToolBar {
+    fn make_tool_bar(&self) -> wx::ToolBar {
         let tb = wx::ToolBar::new(
-            Some(panel),
+            Some(&self.m_panel),
             wx::ID_ANY,
             &wx::Point::default(),
             &wx::Size::default(),
             wx::TB_NODIVIDER.into(),
             "",
         );
-        Self::add_tool_bar_button(&tb, "Help", "wxART_HELP_BOOK");
+        self.add_tool_bar_button(&tb, "Help", "wxART_HELP_BOOK");
         tb.add_separator();
-        Self::add_tool_bar_button(&tb, "Open", "wxART_FILE_OPEN");
+        self.add_tool_bar_button(&tb, "Open", "wxART_FILE_OPEN");
         tb.add_separator();
-        Self::add_tool_bar_button(&tb, "Up", "wxART_GO_DIR_UP");
-        Self::add_tool_bar_button(&tb, "Execute", "wxART_EXECUTABLE_FILE");
+        self.add_tool_bar_button(&tb, "Up", "wxART_GO_DIR_UP");
+        self.add_tool_bar_button(&tb, "Execute", "wxART_EXECUTABLE_FILE");
         tb
     }
 }
