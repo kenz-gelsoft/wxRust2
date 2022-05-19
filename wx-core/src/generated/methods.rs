7bbee7238b16f4e111269098c2e6a8530795bfe6
KENZ <KENZ.gelsoft@gmail.com>
2022-05-20 08:58:13 +0900

    Generate wxStaticBox

--- wx-core/src/generated/methods.rs
+++ wx-core/src/generated/methods.rs
@@ -2544,10 +2544,62 @@ pub trait StaticBitmapMethods: ControlMethods {
     // NOT_SUPPORTED: fn GetScaleMode()
 }
 
+// wxStaticBox
+pub trait StaticBoxMethods: ControlMethods {
+    // DTOR: fn ~wxStaticBox()
+    fn create_str<W: WindowMethods, P: PointMethods, S: SizeMethods>(
+        &self,
+        parent: Option<&W>,
+        id: c_int,
+        label: &str,
+        pos: &P,
+        size: &S,
+        style: c_long,
+        name: &str,
+    ) -> bool {
+        unsafe {
+            let parent = match parent {
+                Some(r) => r.as_ptr(),
+                None => ptr::null_mut(),
+            };
+            let label = wx_base::wx_string_from(label);
+            let pos = pos.as_ptr();
+            let size = size.as_ptr();
+            let name = wx_base::wx_string_from(name);
+            ffi::wxStaticBox_Create(self.as_ptr(), parent, id, label, pos, size, style, name)
+        }
+    }
+    fn create_window<W: WindowMethods, W2: WindowMethods, P: PointMethods, S: SizeMethods>(
+        &self,
+        parent: Option<&W>,
+        id: c_int,
+        label: Option<&W2>,
+        pos: &P,
+        size: &S,
+        style: c_long,
+        name: &str,
+    ) -> bool {
+        unsafe {
+            let parent = match parent {
+                Some(r) => r.as_ptr(),
+                None => ptr::null_mut(),
+            };
+            let label = match label {
+                Some(r) => r.as_ptr(),
+                None => ptr::null_mut(),
+            };
+            let pos = pos.as_ptr();
+            let size = size.as_ptr();
+            let name = wx_base::wx_string_from(name);
+            ffi::wxStaticBox_Create1(self.as_ptr(), parent, id, label, pos, size, style, name)
+        }
+    }
+}
+
 // wxStaticBoxSizer
 pub trait StaticBoxSizerMethods: BoxSizerMethods {
-    fn get_static_box(&self) -> *mut c_void {
-        unsafe { ffi::wxStaticBoxSizer_GetStaticBox(self.as_ptr()) }
+    fn get_static_box(&self) -> WeakRef<StaticBox> {
+        unsafe { WeakRef::<StaticBox>::from(ffi::wxStaticBoxSizer_GetStaticBox(self.as_ptr())) }
     }
 }
 
