1663d5a389f671406ac23f7ca4b0c47ae056604d
KENZ <KENZ.gelsoft@gmail.com>
2022-06-05 12:42:27 +0900

    Implement ColourPicker widget page

--- samples/widgets/src/clrpicker.rs
+++ samples/widgets/src/clrpicker.rs
@@ -6,39 +6,14 @@ use wx::methods::*;
 
 // control ids
 #[derive(Clone, Copy)]
-enum ColourPickerPage {
+enum PickerPage {
     Reset = wx::ID_HIGHEST as isize,
-    Add,
-    AddText,
-    AddSeveral,
-    AddMany,
-    Clear,
-    Change,
-    ChangeText,
-    Delete,
-    DeleteText,
-    DeleteSel,
-    ColourPicker,
-    ContainerTests,
+    Colour,
 }
-impl ColourPickerPage {
+impl PickerPage {
     fn from(v: c_int) -> Option<Self> {
-        use ColourPickerPage::*;
-        for e in [
-            Reset,
-            Add,
-            AddText,
-            AddSeveral,
-            AddMany,
-            Clear,
-            Change,
-            ChangeText,
-            Delete,
-            DeleteText,
-            DeleteSel,
-            ColourPicker,
-            ContainerTests,
-        ] {
+        use PickerPage::*;
+        for e in [Reset, Colour] {
             if v == e.into() {
                 return Some(e);
             }
@@ -46,40 +21,28 @@ impl ColourPickerPage {
         return None;
     }
 }
-impl From<ColourPickerPage> for c_int {
-    fn from(w: ColourPickerPage) -> Self {
+impl From<PickerPage> for c_int {
+    fn from(w: PickerPage) -> Self {
         w as c_int
     }
 }
 
-const CHECKBOX_KIND_2STATE: c_int = 0;
-const CHECKBOX_KIND_3STATE: c_int = 1;
-const CHECKBOX_KIND_3STATE_USER: c_int = 2;
-
 #[derive(Clone)]
 pub struct ConfigUI {
-    // the controls
-    // ------------
-    // the checkboxes
-    chk_sort: wx::CheckBox,
-
-    // sizer
-    sizer_choice: wx::BoxSizer,
-
-    // the text entries for "Add/change string" and "Delete" buttons
-    text_add: wx::TextCtrl,
-    text_change: wx::TextCtrl,
-    text_delete: wx::TextCtrl,
+    // other controls
+    // --------------
+    chk_colour_text_ctrl: wx::CheckBox,
+    chk_colour_show_label: wx::CheckBox,
+    chk_colour_show_alpha: wx::CheckBox,
+    sizer: wx::BoxSizer,
 }
 
 #[derive(Clone)]
 pub struct ColourPickerWidgetsPage {
     pub base: wx::Panel,
     config_ui: RefCell<Option<ConfigUI>>,
-    // the choice itself
-    choice: Rc<RefCell<Option<wx::ColourPickerCtrl>>>,
-
-    s_item: RefCell<c_int>,
+    // the picker
+    clr_picker: Rc<RefCell<Option<wx::ColourPickerCtrl>>>,
 }
 impl WidgetsPage for ColourPickerWidgetsPage {
     fn base(&self) -> &wx::Panel {
@@ -89,194 +52,75 @@ impl WidgetsPage for ColourPickerWidgetsPage {
         return "ColourPicker";
     }
     fn create_content(&self) {
-        // /*
-        //    What we create here is a frame having 3 panes: style pane is the
-        //    leftmost one, in the middle the pane with buttons allowing to perform
-        //    miscellaneous choice operations and the pane containing the choice
-        //    itself to the right
-        // */
-        // let sizer_top = wx::BoxSizer::new(wx::HORIZONTAL);
+        // left pane
+        let boxleft = wx::BoxSizer::new(wx::VERTICAL);
 
-        // // left pane
-        // let s_box = wx::StaticBox::builder(Some(&self.base))
-        //     .label("&Set choice parameters")
-        //     .build();
+        let clrbox =
+            wx::StaticBoxSizer::new_with_int(wx::VERTICAL, Some(&self.base), "&ColourPicker style");
+        let chk_colour_text_ctrl =
+            self.create_check_box_and_add_to_sizer(&clrbox, "With label", wx::ID_ANY);
+        let chk_colour_show_label =
+            self.create_check_box_and_add_to_sizer(&clrbox, "With textctrl", wx::ID_ANY);
+        let chk_colour_show_alpha =
+            self.create_check_box_and_add_to_sizer(&clrbox, "With opacity", wx::ID_ANY);
+        boxleft.add_sizer_int(Some(&clrbox), 0, wx::ALL | wx::GROW, 5, wx::Object::none());
 
-        // let sizer_left = wx::StaticBoxSizer::new_with_staticbox(Some(&s_box), wx::VERTICAL);
+        boxleft.add_window_int(
+            Some(
+                &wx::Button::builder(Some(&self.base))
+                    .id(PickerPage::Reset.into())
+                    .label("&Reset")
+                    .build(),
+            ),
+            0,
+            wx::ALIGN_CENTRE_HORIZONTAL | wx::ALL,
+            15,
+            wx::Object::none(),
+        );
 
-        // let chk_sort =
-        //     self.create_check_box_and_add_to_sizer(&sizer_left, "&Sort items", wx::ID_ANY);
+        self.reset(); // set checkboxes state
 
-        // let btn = wx::Button::builder(Some(&self.base))
-        //     .id(ColourPickerPage::Reset.into())
-        //     .label("&Reset")
-        //     .build();
-        // sizer_left.add_window_int(
-        //     Some(&btn),
-        //     0,
-        //     wx::ALIGN_CENTRE_HORIZONTAL | wx::ALL,
-        //     15,
-        //     wx::Object::none(),
-        // );
+        // create pickers
+        self.create_picker();
 
-        // // middle pane
-        // let s_box2 = wx::StaticBox::builder(Some(&self.base))
-        //     .label("&Change choice contents")
-        //     .build();
-        // let sizer_middle = wx::StaticBoxSizer::new_with_staticbox(Some(&s_box2), wx::VERTICAL);
-
-        // let sizer_row = wx::BoxSizer::new(wx::HORIZONTAL);
-        // let btn = wx::Button::builder(Some(&self.base))
-        //     .id(ColourPickerPage::Add.into())
-        //     .label("&Add this string")
-        //     .build();
-        // let text_add = wx::TextCtrl::builder(Some(&self.base))
-        //     .id(ColourPickerPage::AddText.into())
-        //     .value("test item 0")
-        //     .build();
-        // sizer_row.add_window_int(Some(&btn), 0, wx::RIGHT, 5, wx::Object::none());
-        // sizer_row.add_window_int(Some(&text_add), 1, wx::LEFT, 5, wx::Object::none());
-        // sizer_middle.add_sizer_int(
-        //     Some(&sizer_row),
-        //     0,
-        //     wx::ALL | wx::GROW,
-        //     5,
-        //     wx::Object::none(),
-        // );
-
-        // let btn = wx::Button::builder(Some(&self.base))
-        //     .id(ColourPickerPage::AddSeveral.into())
-        //     .label("&Insert a few strings")
-        //     .build();
-        // sizer_middle.add_window_int(Some(&btn), 0, wx::ALL | wx::GROW, 5, wx::Object::none());
-
-        // let btn = wx::Button::builder(Some(&self.base))
-        //     .id(ColourPickerPage::AddMany.into())
-        //     .label("Add &many strings")
-        //     .build();
-        // sizer_middle.add_window_int(Some(&btn), 0, wx::ALL | wx::GROW, 5, wx::Object::none());
-
-        // let sizer_row = wx::BoxSizer::new(wx::HORIZONTAL);
-        // let btn = wx::Button::builder(Some(&self.base))
-        //     .id(ColourPickerPage::Change.into())
-        //     .label("C&hange current")
-        //     .build();
-        // let text_change = wx::TextCtrl::builder(Some(&self.base))
-        //     .id(ColourPickerPage::ChangeText.into())
-        //     .build();
-        // sizer_row.add_window_int(Some(&btn), 0, wx::RIGHT, 5, wx::Object::none());
-        // sizer_row.add_window_int(Some(&text_change), 1, wx::LEFT, 5, wx::Object::none());
-        // sizer_middle.add_sizer_int(
-        //     Some(&sizer_row),
-        //     0,
-        //     wx::ALL | wx::GROW,
-        //     5,
-        //     wx::Object::none(),
-        // );
-
-        // let sizer_row = wx::BoxSizer::new(wx::HORIZONTAL);
-        // let btn = wx::Button::builder(Some(&self.base))
-        //     .id(ColourPickerPage::Delete.into())
-        //     .label("&Delete this item")
-        //     .build();
-        // let text_delete = wx::TextCtrl::builder(Some(&self.base))
-        //     .id(ColourPickerPage::DeleteText.into())
-        //     .build();
-        // sizer_row.add_window_int(Some(&btn), 0, wx::RIGHT, 5, wx::Object::none());
-        // sizer_row.add_window_int(Some(&text_delete), 1, wx::LEFT, 5, wx::Object::none());
-        // sizer_middle.add_sizer_int(
-        //     Some(&sizer_row),
-        //     0,
-        //     wx::ALL | wx::GROW,
-        //     5,
-        //     wx::Object::none(),
-        // );
-
-        // let btn = wx::Button::builder(Some(&self.base))
-        //     .id(ColourPickerPage::DeleteSel.into())
-        //     .label("Delete &selection")
-        //     .build();
-        // sizer_middle.add_window_int(Some(&btn), 0, wx::ALL | wx::GROW, 5, wx::Object::none());
-
-        // let btn = wx::Button::builder(Some(&self.base))
-        //     .id(ColourPickerPage::Clear.into())
-        //     .label("&Clear")
-        //     .build();
-        // sizer_middle.add_window_int(Some(&btn), 0, wx::ALL | wx::GROW, 5, wx::Object::none());
-
-        // let btn = wx::Button::builder(Some(&self.base))
-        //     .id(ColourPickerPage::ContainerTests.into())
-        //     .label("Run &tests")
-        //     .build();
-        // sizer_middle.add_window_int(Some(&btn), 0, wx::ALL | wx::GROW, 5, wx::Object::none());
-
-        // // right pane
-        // let sizer_right = wx::BoxSizer::new(wx::VERTICAL);
-        // let choice = wx::ColourPicker::builder(Some(&self.base))
-        //     .id(ColourPickerPage::ColourPicker.into())
-        //     .build();
-        // sizer_right.add_window_int(Some(&choice), 0, wx::ALL | wx::GROW, 5, wx::Object::none());
-        // sizer_right.set_min_size_int(150, 0);
-        // *self.choice.borrow_mut() = Some(choice);
-
-        // // the 3 panes panes compose the window
-        // sizer_top.add_sizer_sizerflags(
-        //     Some(&sizer_left),
-        //     wx::SizerFlags::new(0)
-        //         .expand()
-        //         .double_border(wx::ALL & !wx::LEFT),
-        // );
-        // sizer_top.add_sizer_sizerflags(
-        //     Some(&sizer_middle),
-        //     wx::SizerFlags::new(1).expand().double_border(wx::ALL),
-        // );
-        // sizer_top.add_sizer_sizerflags(
-        //     Some(&sizer_right),
-        //     wx::SizerFlags::new(1)
-        //         .expand()
-        //         .double_border(wx::ALL & !wx::RIGHT),
-        // );
-        // *self.config_ui.borrow_mut() = Some(ConfigUI {
-        //     chk_sort,
-
-        //     sizer_choice: sizer_right, // save it to modify it later
-
-        //     text_add,
-        //     text_change,
-        //     text_delete,
-        // });
-
-        // // do create the main control
-        // self.reset();
-        // self.create_choice();
-
-        // self.base.set_sizer(Some(&sizer_top), true);
-    }
-
-    fn handle_button(&self, event: &wx::CommandEvent) {
-        println!("event={}", event.get_id());
-        if let Some(m) = ColourPickerPage::from(event.get_id()) {
-            if let Some(config_ui) = self.config_ui.borrow().as_ref() {
-                match m {
-                    ColourPickerPage::Reset => self.on_button_reset(),
-                    ColourPickerPage::Change => self.on_button_change(config_ui),
-                    ColourPickerPage::Delete => self.on_button_delete(config_ui),
-                    ColourPickerPage::DeleteSel => self.on_button_delete_sel(),
-                    ColourPickerPage::Clear => self.on_button_clear(),
-                    ColourPickerPage::Add => self.on_button_add(config_ui),
-                    ColourPickerPage::AddSeveral => self.on_button_add_several(),
-                    ColourPickerPage::AddMany => self.on_button_add_many(),
-                    // TODO: Support update ui event to disable this when not 3state
-                    _ => (),
-                };
-            }
+        // right pane
+        let sizer = wx::BoxSizer::new(wx::VERTICAL);
+        sizer.add_int_int(1, 1, 1, wx::GROW | wx::ALL, 5, wx::Object::none()); // spacer
+        if let Some(clr_picker) = self.clr_picker.borrow().as_ref() {
+            sizer.add_window_int(
+                Some(clr_picker),
+                0,
+                wx::ALIGN_CENTER | wx::ALL,
+                5,
+                wx::Object::none(),
+            );
         }
+        sizer.add_int_int(1, 1, 1, wx::GROW | wx::ALL, 5, wx::Object::none()); // spacer
+
+        // global pane
+        let sz = wx::BoxSizer::new(wx::HORIZONTAL);
+        sz.add_sizer_int(Some(&boxleft), 0, wx::GROW | wx::ALL, 5, wx::Object::none());
+        sz.add_sizer_int(Some(&sizer), 1, wx::GROW | wx::ALL, 5, wx::Object::none());
+
+        *self.config_ui.borrow_mut() = Some(ConfigUI {
+            chk_colour_text_ctrl,
+            chk_colour_show_label,
+            chk_colour_show_alpha,
+
+            sizer,
+        });
+
+        self.base.set_sizer(Some(&sz), true);
     }
-    fn handle_checkbox(&self, event: &wx::CommandEvent) {
-        self.on_check_or_radio_box();
+
+    fn handle_button(&self, _: &wx::CommandEvent) {
+        self.on_button_reset();
+    }
+    fn handle_checkbox(&self, _: &wx::CommandEvent) {
+        self.on_check_box();
     }
     fn handle_radiobox(&self, _: &wx::CommandEvent) {
-        self.on_check_or_radio_box();
+        // Do nothing
     }
 }
 impl ColourPickerWidgetsPage {
@@ -287,142 +131,77 @@ impl ColourPickerWidgetsPage {
         ColourPickerWidgetsPage {
             base: panel,
             config_ui: RefCell::new(None),
-            choice: Rc::new(RefCell::new(None)),
-            s_item: RefCell::new(1),
+            clr_picker: Rc::new(RefCell::new(None)),
         }
     }
 
     fn recreate_widget(&self) {
-        self.create_choice();
+        self.create_picker();
+
+        if let Some(config_ui) = self.config_ui.borrow().as_ref() {
+            // MEMO: Destroy()ing in create_picker() removes from its sizer.
+            // config_ui.sizer.remove_int(1);
+            self.create_picker();
+            if let Some(clr_pickr) = self.clr_picker.borrow().as_ref() {
+                config_ui.sizer.insert_window_int(
+                    1,
+                    Some(clr_pickr),
+                    0,
+                    wx::ALIGN_CENTER | wx::ALL,
+                    5,
+                    wx::Object::none(),
+                );
+            }
+            config_ui.sizer.layout();
+        }
     }
 
     fn reset(&self) {
         if let Some(config_ui) = self.config_ui.borrow().as_ref() {
-            config_ui.chk_sort.set_value(false);
+            config_ui
+                .chk_colour_text_ctrl
+                .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_USE_TEXTCTRL) != 0);
+            config_ui
+                .chk_colour_show_label
+                .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_SHOW_LABEL) != 0);
+            config_ui
+                .chk_colour_show_alpha
+                .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_SHOW_ALPHA) != 0);
         }
     }
 
-    fn create_choice(&self) {
-        // if let Some(config_ui) = self.config_ui.borrow().as_ref() {
-        //     let mut flags = wx::BORDER_DEFAULT;
+    fn create_picker(&self) {
+        if let Some(clr_picker) = self.clr_picker.borrow().as_ref() {
+            clr_picker.destroy();
+        }
 
-        //     if config_ui.chk_sort.is_checked() {
-        //         flags |= wx::CB_SORT as c_long;
-        //     }
+        let mut style = wx::BORDER_DEFAULT;
+        if let Some(config_ui) = self.config_ui.borrow().as_ref() {
+            if config_ui.chk_colour_text_ctrl.get_value() {
+                style |= wx::CLRP_USE_TEXTCTRL as c_long;
+            }
+            if config_ui.chk_colour_show_label.get_value() {
+                style |= wx::CLRP_SHOW_LABEL as c_long;
+            }
+            if config_ui.chk_colour_show_alpha.get_value() {
+                style |= wx::CLRP_SHOW_ALPHA as c_long;
+            }
+        }
 
-        //     let items = wx::ArrayString::new();
-        //     if let Some(choice) = self.choice.borrow().as_ref() {
-        //         // TODO: remove (and delete) all choicees
-        //         let count = choice.get_count();
-        //         for n in 0..count {
-        //             items.add(&choice.get_string(n));
-        //         }
-
-        //         config_ui.sizer_choice.detach_window(Some(choice));
-        //         choice.destroy();
-        //     }
-
-        //     let new_choice = wx::ColourPicker::builder(Some(&self.base))
-        //         .id(ColourPickerPage::ColourPicker.into())
-        //         .style(flags)
-        //         .build();
-        //     new_choice.set(&items);
-
-        //     let sizer_choice = &config_ui.sizer_choice;
-        //     sizer_choice.add_window_int(
-        //         Some(&new_choice),
-        //         0,
-        //         wx::GROW | wx::ALL,
-        //         5,
-        //         wx::Object::none(),
-        //     );
-        //     sizer_choice.layout();
-
-        //     *self.choice.borrow_mut() = Some(new_choice);
-        // }
+        let clr_picker = wx::ColourPickerCtrl::builder(Some(&self.base))
+            .id(PickerPage::Colour.into())
+            .colour(wx::Colour::new_with_str("RED"))
+            .style(style)
+            .build();
+        *self.clr_picker.borrow_mut() = Some(clr_picker);
     }
 
     fn on_button_reset(&self) {
         self.reset();
-        self.create_choice();
+        self.recreate_widget();
     }
 
-    fn on_button_change(&self, config_ui: &ConfigUI) {
-        // if let Some(choice) = self.choice.borrow().as_ref() {
-        //     let selection = choice.get_selection();
-        //     if selection != wx::NOT_FOUND {
-        //         choice.set_string(
-        //             selection.try_into().unwrap(),
-        //             &config_ui.text_change.get_value(),
-        //         );
-        //     }
-        // }
-    }
-
-    fn on_button_delete(&self, config_ui: &ConfigUI) {
-        // let n = config_ui.text_delete.get_value();
-        // if let Ok(n) = n.parse() {
-        //     if let Some(choice) = self.choice.borrow().as_ref() {
-        //         if n < choice.get_count() {
-        //             choice.delete(n);
-        //         }
-        //     }
-        // }
-    }
-
-    fn on_button_delete_sel(&self) {
-        // if let Some(choice) = self.choice.borrow().as_ref() {
-        //     let selection = choice.get_selection();
-        //     if selection != wx::NOT_FOUND {
-        //         choice.delete(selection.try_into().unwrap());
-        //     }
-        // }
-    }
-
-    fn on_button_clear(&self) {
-        // if let Some(choice) = self.choice.borrow().as_ref() {
-        //     choice.clear();
-        // }
-    }
-
-    fn on_button_add(&self, config_ui: &ConfigUI) {
-        // let s = config_ui.text_add.get_value();
-        // if !config_ui.text_add.is_modified() {
-        //     // update the default string
-        //     let s_item = *self.s_item.borrow();
-        //     config_ui
-        //         .text_add
-        //         .set_value(&format!("test item {}", s_item));
-        //     *self.s_item.borrow_mut() = s_item + 1;
-        // }
-
-        // if let Some(choice) = self.choice.borrow().as_ref() {
-        //     choice.append_str(&s);
-        // }
-    }
-
-    fn on_button_add_many(&self) {
-        // // "many" means 1000 here
-        // let strings = wx::ArrayString::new();
-        // for n in 0..1000 {
-        //     strings.add(&format!("item #{}", n));
-        // }
-        // if let Some(choice) = self.choice.borrow().as_ref() {
-        //     choice.append_arraystring(&strings);
-        // }
-    }
-
-    fn on_button_add_several(&self) {
-        // let items = wx::ArrayString::new();
-        // items.add("First");
-        // items.add("another one");
-        // items.add("and the last (very very very very very very very very very very long) one");
-        // if let Some(choice) = self.choice.borrow().as_ref() {
-        //     choice.insert_arraystring(&items, 0);
-        // }
-    }
-
-    fn on_check_or_radio_box(&self) {
-        self.create_choice();
+    fn on_check_box(&self) {
+        self.recreate_widget();
     }
 }
