72b849616831dd55ac8da53960a4786137a704f6
KENZ <KENZ.gelsoft@gmail.com>
2022-06-12 18:31:18 +0900

    Generate wxDateTime binding

--- wx-base/src/generated.rs
+++ wx-base/src/generated.rs
@@ -14,6 +14,151 @@ use crate::wx_class;
 mod ffi;
 pub mod methods;
 
+// wxDateTime
+wx_class! { DateTime =
+    DateTimeIsOwned<true>(wxDateTime) impl
+        DateTimeMethods
+}
+impl<const OWNED: bool> DateTimeIsOwned<OWNED> {
+    //  ENUM: TZ
+    pub const Local: c_int = 0;
+    pub const GMT_12: c_int = 0 + 1;
+    pub const GMT_11: c_int = 0 + 2;
+    pub const GMT_10: c_int = 0 + 3;
+    pub const GMT_9: c_int = 0 + 4;
+    pub const GMT_8: c_int = 0 + 5;
+    pub const GMT_7: c_int = 0 + 6;
+    pub const GMT_6: c_int = 0 + 7;
+    pub const GMT_5: c_int = 0 + 8;
+    pub const GMT_4: c_int = 0 + 9;
+    pub const GMT_3: c_int = 0 + 10;
+    pub const GMT_2: c_int = 0 + 11;
+    pub const GMT_1: c_int = 0 + 12;
+    pub const GMT0: c_int = 0 + 13;
+    pub const GMT1: c_int = 0 + 14;
+    pub const GMT2: c_int = 0 + 15;
+    pub const GMT3: c_int = 0 + 16;
+    pub const GMT4: c_int = 0 + 17;
+    pub const GMT5: c_int = 0 + 18;
+    pub const GMT6: c_int = 0 + 19;
+    pub const GMT7: c_int = 0 + 20;
+    pub const GMT8: c_int = 0 + 21;
+    pub const GMT9: c_int = 0 + 22;
+    pub const GMT10: c_int = 0 + 23;
+    pub const GMT11: c_int = 0 + 24;
+    pub const GMT12: c_int = 0 + 25;
+    pub const GMT13: c_int = 0 + 26;
+    pub const WET: c_int = GMT0;
+    pub const WEST: c_int = GMT1;
+    pub const CET: c_int = GMT1;
+    pub const CEST: c_int = GMT2;
+    pub const EET: c_int = GMT2;
+    pub const EEST: c_int = GMT3;
+    pub const MSK: c_int = GMT3;
+    pub const MSD: c_int = GMT4;
+    pub const AST: c_int = GMT_4;
+    pub const ADT: c_int = GMT_3;
+    pub const EST: c_int = GMT_5;
+    pub const EDT: c_int = GMT_4;
+    pub const CST: c_int = GMT_6;
+    pub const CDT: c_int = GMT_5;
+    pub const MST: c_int = GMT_7;
+    pub const MDT: c_int = GMT_6;
+    pub const PST: c_int = GMT_8;
+    pub const PDT: c_int = GMT_7;
+    pub const HST: c_int = GMT_10;
+    pub const AKST: c_int = GMT_9;
+    pub const AKDT: c_int = GMT_8;
+    pub const A_WST: c_int = GMT8;
+    pub const A_CST: c_int = GMT13 + 1;
+    pub const A_EST: c_int = GMT10;
+    pub const A_ESST: c_int = GMT11;
+    pub const NZST: c_int = GMT12;
+    pub const NZDT: c_int = GMT13;
+    pub const UTC: c_int = GMT0;
+
+    //  ENUM: Calendar
+    pub const Gregorian: c_int = 0;
+    pub const Julian: c_int = 0 + 1;
+
+    //  ENUM: Country
+    pub const Country_Unknown: c_int = 0;
+    pub const Country_Default: c_int = 0 + 1;
+    pub const Country_WesternEurope_Start: c_int = 0 + 2;
+    pub const Country_EEC: c_int = Country_WesternEurope_Start;
+    pub const France: c_int = Country_WesternEurope_Start + 1;
+    pub const Germany: c_int = Country_WesternEurope_Start + 2;
+    pub const UK: c_int = Country_WesternEurope_Start + 3;
+    pub const Country_WesternEurope_End: c_int = UK;
+    pub const Russia: c_int = UK + 1;
+    pub const USA: c_int = UK + 2;
+
+    //  ENUM: Month
+    pub const Jan: c_int = 0;
+    pub const Feb: c_int = 0 + 1;
+    pub const Mar: c_int = 0 + 2;
+    pub const Apr: c_int = 0 + 3;
+    pub const May: c_int = 0 + 4;
+    pub const Jun: c_int = 0 + 5;
+    pub const Jul: c_int = 0 + 6;
+    pub const Aug: c_int = 0 + 7;
+    pub const Sep: c_int = 0 + 8;
+    pub const Oct: c_int = 0 + 9;
+    pub const Nov: c_int = 0 + 10;
+    pub const Dec: c_int = 0 + 11;
+    pub const Inv_Month: c_int = 0 + 12;
+
+    //  ENUM: WeekDay
+    pub const Sun: c_int = 0;
+    pub const Mon: c_int = 0 + 1;
+    pub const Tue: c_int = 0 + 2;
+    pub const Wed: c_int = 0 + 3;
+    pub const Thu: c_int = 0 + 4;
+    pub const Fri: c_int = 0 + 5;
+    pub const Sat: c_int = 0 + 6;
+    pub const Inv_WeekDay: c_int = 0 + 7;
+
+    //  ENUM: Year
+    pub const Inv_Year: c_int = SHRT_MIN;
+
+    //  ENUM: NameFlags
+    pub const Name_Full: c_int = 0x01;
+    pub const Name_Abbr: c_int = 0x02;
+
+    //  ENUM: WeekFlags
+    pub const Default_First: c_int = 0;
+    pub const Monday_First: c_int = 0 + 1;
+    pub const Sunday_First: c_int = 0 + 2;
+
+    pub fn new() -> DateTimeIsOwned<OWNED> {
+        unsafe { DateTimeIsOwned(ffi::wxDateTime_new()) }
+    }
+    pub fn new_with_datetime<D: DateTimeMethods>(date: &D) -> DateTimeIsOwned<OWNED> {
+        unsafe {
+            let date = date.as_ptr();
+            DateTimeIsOwned(ffi::wxDateTime_new1(date))
+        }
+    }
+    // NOT_SUPPORTED: fn wxDateTime2()
+    // BLOCKED: fn wxDateTime3()
+    pub fn new_with_double(jdn: c_double) -> DateTimeIsOwned<OWNED> {
+        unsafe { DateTimeIsOwned(ffi::wxDateTime_new4(jdn)) }
+    }
+    // NOT_SUPPORTED: fn wxDateTime5()
+    // NOT_SUPPORTED: fn wxDateTime6()
+    // BLOCKED: fn wxDateTime7()
+    pub fn none() -> Option<&'static Self> {
+        None
+    }
+}
+impl<const OWNED: bool> Drop for DateTimeIsOwned<OWNED> {
+    fn drop(&mut self) {
+        if OWNED {
+            unsafe { ffi::wxDateTime_delete(self.0) }
+        }
+    }
+}
+
 // wxEvent
 wx_class! { Event =
     EventIsOwned<true>(wxEvent) impl
--- wx-base/src/generated/methods.rs
+++ wx-base/src/generated/methods.rs
@@ -21,6 +21,364 @@ pub trait WxRustMethods {
     }
 }
 
+// wxDateTime
+pub trait DateTimeMethods: WxRustMethods {
+    fn reset_time(&self) -> &Self {
+        unsafe {
+            ffi::wxDateTime_ResetTime(self.as_ptr());
+            &self
+        }
+    }
+    // NOT_SUPPORTED: fn Set()
+    // BLOCKED: fn Set1()
+    fn set_tm(&self, tm: *const c_void) -> &Self {
+        unsafe {
+            ffi::wxDateTime_Set2(self.as_ptr(), tm);
+            &self
+        }
+    }
+    fn set_double(&self, jdn: c_double) -> &Self {
+        unsafe {
+            ffi::wxDateTime_Set3(self.as_ptr(), jdn);
+            &self
+        }
+    }
+    // NOT_SUPPORTED: fn Set4()
+    // NOT_SUPPORTED: fn Set5()
+    // NOT_SUPPORTED: fn SetDay()
+    // NOT_SUPPORTED: fn SetFromDOS()
+    // NOT_SUPPORTED: fn SetHour()
+    // NOT_SUPPORTED: fn SetMillisecond()
+    // NOT_SUPPORTED: fn SetMinute()
+    // NOT_SUPPORTED: fn SetMonth()
+    // NOT_SUPPORTED: fn SetSecond()
+    fn set_to_current(&self) -> &Self {
+        unsafe {
+            ffi::wxDateTime_SetToCurrent(self.as_ptr());
+            &self
+        }
+    }
+    fn set_year(&self, year: c_int) -> &Self {
+        unsafe {
+            ffi::wxDateTime_SetYear(self.as_ptr(), year);
+            &self
+        }
+    }
+    // NOT_SUPPORTED: fn operator=()
+    // BLOCKED: fn operator=1()
+    // NOT_SUPPORTED: fn GetAsDOS()
+    fn set_from_msw_sys_time(&self, st: *const c_void) -> &Self {
+        unsafe {
+            ffi::wxDateTime_SetFromMSWSysTime(self.as_ptr(), st);
+            &self
+        }
+    }
+    fn get_as_msw_sys_time(&self, st: *mut c_void) {
+        unsafe { ffi::wxDateTime_GetAsMSWSysTime(self.as_ptr(), st) }
+    }
+    fn get_century_timezone(&self, tz: *const c_void) -> c_int {
+        unsafe { ffi::wxDateTime_GetCentury(self.as_ptr(), tz) }
+    }
+    fn get_date_only(&self) -> DateTime {
+        unsafe { DateTimeIsOwned(ffi::wxDateTime_GetDateOnly(self.as_ptr())) }
+    }
+    // NOT_SUPPORTED: fn GetDay()
+    // NOT_SUPPORTED: fn GetDayOfYear()
+    // NOT_SUPPORTED: fn GetHour()
+    // NOT_SUPPORTED: fn GetMillisecond()
+    // NOT_SUPPORTED: fn GetMinute()
+    // NOT_SUPPORTED: fn GetMonth()
+    // NOT_SUPPORTED: fn GetSecond()
+    // NOT_SUPPORTED: fn GetTicks()
+    // NOT_SUPPORTED: fn GetValue()
+    // NOT_SUPPORTED: fn GetTm()
+    // NOT_SUPPORTED: fn GetWeekDay()
+    fn get_week_based_year(&self, tz: *const c_void) -> c_int {
+        unsafe { ffi::wxDateTime_GetWeekBasedYear(self.as_ptr(), tz) }
+    }
+    // NOT_SUPPORTED: fn GetWeekOfMonth()
+    // NOT_SUPPORTED: fn GetWeekOfYear()
+    fn get_year(&self, tz: *const c_void) -> c_int {
+        unsafe { ffi::wxDateTime_GetYear(self.as_ptr(), tz) }
+    }
+    fn is_valid(&self) -> bool {
+        unsafe { ffi::wxDateTime_IsValid(self.as_ptr()) }
+    }
+    // NOT_SUPPORTED: fn IsWorkDay()
+    fn is_earlier_than<D: DateTimeMethods>(&self, datetime: &D) -> bool {
+        unsafe {
+            let datetime = datetime.as_ptr();
+            ffi::wxDateTime_IsEarlierThan(self.as_ptr(), datetime)
+        }
+    }
+    fn is_equal_to<D: DateTimeMethods>(&self, datetime: &D) -> bool {
+        unsafe {
+            let datetime = datetime.as_ptr();
+            ffi::wxDateTime_IsEqualTo(self.as_ptr(), datetime)
+        }
+    }
+    fn is_equal_up_to<D: DateTimeMethods>(&self, dt: &D, ts: *const c_void) -> bool {
+        unsafe {
+            let dt = dt.as_ptr();
+            ffi::wxDateTime_IsEqualUpTo(self.as_ptr(), dt, ts)
+        }
+    }
+    fn is_later_than<D: DateTimeMethods>(&self, datetime: &D) -> bool {
+        unsafe {
+            let datetime = datetime.as_ptr();
+            ffi::wxDateTime_IsLaterThan(self.as_ptr(), datetime)
+        }
+    }
+    fn is_same_date<D: DateTimeMethods>(&self, dt: &D) -> bool {
+        unsafe {
+            let dt = dt.as_ptr();
+            ffi::wxDateTime_IsSameDate(self.as_ptr(), dt)
+        }
+    }
+    fn is_same_time<D: DateTimeMethods>(&self, dt: &D) -> bool {
+        unsafe {
+            let dt = dt.as_ptr();
+            ffi::wxDateTime_IsSameTime(self.as_ptr(), dt)
+        }
+    }
+    fn is_strictly_between<D: DateTimeMethods, D2: DateTimeMethods>(
+        &self,
+        t1: &D,
+        t2: &D2,
+    ) -> bool {
+        unsafe {
+            let t1 = t1.as_ptr();
+            let t2 = t2.as_ptr();
+            ffi::wxDateTime_IsStrictlyBetween(self.as_ptr(), t1, t2)
+        }
+    }
+    fn is_between<D: DateTimeMethods, D2: DateTimeMethods>(&self, t1: &D, t2: &D2) -> bool {
+        unsafe {
+            let t1 = t1.as_ptr();
+            let t2 = t2.as_ptr();
+            ffi::wxDateTime_IsBetween(self.as_ptr(), t1, t2)
+        }
+    }
+    fn add_datespan(&self, diff: *const c_void) -> DateTime {
+        unsafe { DateTimeIsOwned(ffi::wxDateTime_Add(self.as_ptr(), diff)) }
+    }
+    fn add_datespan(&self, diff: *const c_void) -> &Self {
+        unsafe {
+            ffi::wxDateTime_Add1(self.as_ptr(), diff);
+            &self
+        }
+    }
+    fn add_timespan(&self, diff: *const c_void) -> DateTime {
+        unsafe { DateTimeIsOwned(ffi::wxDateTime_Add2(self.as_ptr(), diff)) }
+    }
+    fn add_timespan(&self, diff: *const c_void) -> &Self {
+        unsafe {
+            ffi::wxDateTime_Add3(self.as_ptr(), diff);
+            &self
+        }
+    }
+    fn subtract_timespan(&self, diff: *const c_void) -> DateTime {
+        unsafe { DateTimeIsOwned(ffi::wxDateTime_Subtract(self.as_ptr(), diff)) }
+    }
+    fn subtract_timespan(&self, diff: *const c_void) -> &Self {
+        unsafe {
+            ffi::wxDateTime_Subtract1(self.as_ptr(), diff);
+            &self
+        }
+    }
+    fn subtract_datespan(&self, diff: *const c_void) -> DateTime {
+        unsafe { DateTimeIsOwned(ffi::wxDateTime_Subtract2(self.as_ptr(), diff)) }
+    }
+    fn subtract_datespan(&self, diff: *const c_void) -> &Self {
+        unsafe {
+            ffi::wxDateTime_Subtract3(self.as_ptr(), diff);
+            &self
+        }
+    }
+    // NOT_SUPPORTED: fn Subtract4()
+    // NOT_SUPPORTED: fn DiffAsDateSpan()
+    // BLOCKED: fn operator+=()
+    // BLOCKED: fn operator+()
+    // BLOCKED: fn operator-=()
+    // BLOCKED: fn operator-()
+    // BLOCKED: fn operator+=1()
+    // BLOCKED: fn operator+1()
+    // BLOCKED: fn operator-=1()
+    // BLOCKED: fn operator-1()
+    // NOT_SUPPORTED: fn operator-2()
+    fn format(&self, format: &str, tz: *const c_void) -> String {
+        unsafe {
+            let format = wx_base::wx_string_from(format);
+            wx_base::from_wx_string(ffi::wxDateTime_Format(self.as_ptr(), format, tz))
+        }
+    }
+    fn format_date(&self) -> String {
+        unsafe { wx_base::from_wx_string(ffi::wxDateTime_FormatDate(self.as_ptr())) }
+    }
+    // NOT_SUPPORTED: fn FormatISOCombined()
+    fn format_iso_date(&self) -> String {
+        unsafe { wx_base::from_wx_string(ffi::wxDateTime_FormatISODate(self.as_ptr())) }
+    }
+    fn format_iso_time(&self) -> String {
+        unsafe { wx_base::from_wx_string(ffi::wxDateTime_FormatISOTime(self.as_ptr())) }
+    }
+    fn format_time(&self) -> String {
+        unsafe { wx_base::from_wx_string(ffi::wxDateTime_FormatTime(self.as_ptr())) }
+    }
+    fn parse_date(&self, date: &str, end: *mut c_void) -> bool {
+        unsafe {
+            let date = wx_base::wx_string_from(date);
+            ffi::wxDateTime_ParseDate(self.as_ptr(), date, end)
+        }
+    }
+    fn parse_date_time(&self, datetime: &str, end: *mut c_void) -> bool {
+        unsafe {
+            let datetime = wx_base::wx_string_from(datetime);
+            ffi::wxDateTime_ParseDateTime(self.as_ptr(), datetime, end)
+        }
+    }
+    fn parse_format<D: DateTimeMethods>(
+        &self,
+        date: &str,
+        format: &str,
+        date_def: &D,
+        end: *mut c_void,
+    ) -> bool {
+        unsafe {
+            let date = wx_base::wx_string_from(date);
+            let format = wx_base::wx_string_from(format);
+            let date_def = date_def.as_ptr();
+            ffi::wxDateTime_ParseFormat(self.as_ptr(), date, format, date_def, end)
+        }
+    }
+    // BLOCKED: fn ParseFormat1()
+    // BLOCKED: fn ParseFormat2()
+    // NOT_SUPPORTED: fn ParseISOCombined()
+    fn parse_iso_date(&self, date: &str) -> bool {
+        unsafe {
+            let date = wx_base::wx_string_from(date);
+            ffi::wxDateTime_ParseISODate(self.as_ptr(), date)
+        }
+    }
+    fn parse_iso_time(&self, date: &str) -> bool {
+        unsafe {
+            let date = wx_base::wx_string_from(date);
+            ffi::wxDateTime_ParseISOTime(self.as_ptr(), date)
+        }
+    }
+    fn parse_rfc822_date(&self, date: &str, end: *mut c_void) -> bool {
+        unsafe {
+            let date = wx_base::wx_string_from(date);
+            ffi::wxDateTime_ParseRfc822Date(self.as_ptr(), date, end)
+        }
+    }
+    fn parse_time(&self, time: &str, end: *mut c_void) -> bool {
+        unsafe {
+            let time = wx_base::wx_string_from(time);
+            ffi::wxDateTime_ParseTime(self.as_ptr(), time, end)
+        }
+    }
+    // NOT_SUPPORTED: fn GetLastMonthDay()
+    // NOT_SUPPORTED: fn GetLastWeekDay()
+    // NOT_SUPPORTED: fn GetNextWeekDay()
+    // NOT_SUPPORTED: fn GetPrevWeekDay()
+    // NOT_SUPPORTED: fn GetWeekDay1()
+    // NOT_SUPPORTED: fn GetWeekDayInSameWeek()
+    // NOT_SUPPORTED: fn GetYearDay()
+    // NOT_SUPPORTED: fn SetToLastMonthDay()
+    // NOT_SUPPORTED: fn SetToLastWeekDay()
+    // NOT_SUPPORTED: fn SetToNextWeekDay()
+    // NOT_SUPPORTED: fn SetToPrevWeekDay()
+    // NOT_SUPPORTED: fn SetToWeekDay()
+    // NOT_SUPPORTED: fn SetToWeekDayInSameWeek()
+    // NOT_SUPPORTED: fn SetToYearDay()
+    fn get_jdn(&self) -> c_double {
+        unsafe { ffi::wxDateTime_GetJDN(self.as_ptr()) }
+    }
+    fn get_julian_day_number(&self) -> c_double {
+        unsafe { ffi::wxDateTime_GetJulianDayNumber(self.as_ptr()) }
+    }
+    fn get_mjd(&self) -> c_double {
+        unsafe { ffi::wxDateTime_GetMJD(self.as_ptr()) }
+    }
+    fn get_modified_julian_day_number(&self) -> c_double {
+        unsafe { ffi::wxDateTime_GetModifiedJulianDayNumber(self.as_ptr()) }
+    }
+    fn get_rata_die(&self) -> c_double {
+        unsafe { ffi::wxDateTime_GetRataDie(self.as_ptr()) }
+    }
+    fn from_timezone(&self, tz: *const c_void, no_dst: bool) -> DateTime {
+        unsafe { DateTimeIsOwned(ffi::wxDateTime_FromTimezone(self.as_ptr(), tz, no_dst)) }
+    }
+    // NOT_SUPPORTED: fn IsDST()
+    fn make_from_timezone(&self, tz: *const c_void, no_dst: bool) -> &Self {
+        unsafe {
+            ffi::wxDateTime_MakeFromTimezone(self.as_ptr(), tz, no_dst);
+            &self
+        }
+    }
+    fn make_timezone(&self, tz: *const c_void, no_dst: bool) -> &Self {
+        unsafe {
+            ffi::wxDateTime_MakeTimezone(self.as_ptr(), tz, no_dst);
+            &self
+        }
+    }
+    fn make_utc(&self, no_dst: bool) -> &Self {
+        unsafe {
+            ffi::wxDateTime_MakeUTC(self.as_ptr(), no_dst);
+            &self
+        }
+    }
+    fn to_timezone(&self, tz: *const c_void, no_dst: bool) -> DateTime {
+        unsafe { DateTimeIsOwned(ffi::wxDateTime_ToTimezone(self.as_ptr(), tz, no_dst)) }
+    }
+    fn to_utc(&self, no_dst: bool) -> DateTime {
+        unsafe { DateTimeIsOwned(ffi::wxDateTime_ToUTC(self.as_ptr(), no_dst)) }
+    }
+    fn convert_year_to_bc(year: c_int) -> c_int {
+        unsafe { ffi::wxDateTime_ConvertYearToBC(year) }
+    }
+    fn get_am_pm_strings(am: *mut c_void, pm: *mut c_void) {
+        unsafe { ffi::wxDateTime_GetAmPmStrings(am, pm) }
+    }
+    // NOT_SUPPORTED: fn GetBeginDST()
+    // NOT_SUPPORTED: fn GetEndDST()
+    fn get_century_int(year: c_int) -> c_int {
+        unsafe { ffi::wxDateTime_GetCentury1(year) }
+    }
+    // NOT_SUPPORTED: fn GetCountry()
+    // NOT_SUPPORTED: fn GetCurrentMonth()
+    // NOT_SUPPORTED: fn GetCurrentYear()
+    // NOT_SUPPORTED: fn GetEnglishMonthName()
+    // NOT_SUPPORTED: fn GetEnglishWeekDayName()
+    // NOT_SUPPORTED: fn GetMonthName()
+    // NOT_SUPPORTED: fn GetNumberOfDays()
+    // NOT_SUPPORTED: fn GetNumberOfDays1()
+    // NOT_SUPPORTED: fn GetTimeNow()
+    // BLOCKED: fn GetTmNow()
+    fn get_tm_now() -> *mut c_void {
+        unsafe { ffi::wxDateTime_GetTmNow1() }
+    }
+    // NOT_SUPPORTED: fn GetWeekDayName()
+    // NOT_SUPPORTED: fn IsDSTApplicable()
+    fn get_first_week_day(first_day: *mut c_void) -> bool {
+        unsafe { ffi::wxDateTime_GetFirstWeekDay(first_day) }
+    }
+    // NOT_SUPPORTED: fn IsLeapYear()
+    // NOT_SUPPORTED: fn IsWestEuropeanCountry()
+    fn now() -> DateTime {
+        unsafe { DateTimeIsOwned(ffi::wxDateTime_Now()) }
+    }
+    // NOT_SUPPORTED: fn SetCountry()
+    // NOT_SUPPORTED: fn SetToWeekOfYear()
+    fn today() -> DateTime {
+        unsafe { DateTimeIsOwned(ffi::wxDateTime_Today()) }
+    }
+    fn u_now() -> DateTime {
+        unsafe { DateTimeIsOwned(ffi::wxDateTime_UNow()) }
+    }
+}
+
 // wxEvent
 pub trait EventMethods: ObjectMethods {
     fn clone(&self) -> Event {
