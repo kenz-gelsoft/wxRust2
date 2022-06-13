68d6291bc11455094336c75123cfdc20cc115c89
KENZ <KENZ.gelsoft@gmail.com>
2022-06-14 07:02:17 +0900

    widgets: Implement DatePicker page create_content

--- samples/widgets/src/datepicker.rs
+++ samples/widgets/src/datepicker.rs
@@ -34,9 +34,9 @@ impl From<DatePickerPage> for c_int {
 pub struct ConfigUI {
     // other controls
     // --------------
-    chk_colour_text_ctrl: wx::CheckBox,
-    chk_colour_show_label: wx::CheckBox,
-    chk_colour_show_alpha: wx::CheckBox,
+    // chk_colour_text_ctrl: wx::CheckBox,
+    // chk_colour_show_label: wx::CheckBox,
+    // chk_colour_show_alpha: wx::CheckBox,
     sizer: wx::BoxSizer,
 }
 
@@ -55,65 +55,148 @@ impl WidgetsPage for DatePickerWidgetsPage {
         return "DatePicker";
     }
     fn create_content(&self) {
-        // left pane
-        let boxleft = wx::BoxSizer::new(wx::VERTICAL);
+        let sizer_top = wx::BoxSizer::new(wx::HORIZONTAL);
 
-        let clrbox =
-            wx::StaticBoxSizer::new_with_int(wx::VERTICAL, Some(&self.base), "&DatePicker style");
-        let chk_colour_text_ctrl =
-            self.create_check_box_and_add_to_sizer(&clrbox, "With label", wx::ID_ANY);
-        let chk_colour_show_label =
-            self.create_check_box_and_add_to_sizer(&clrbox, "With textctrl", wx::ID_ANY);
-        let chk_colour_show_alpha =
-            self.create_check_box_and_add_to_sizer(&clrbox, "With opacity", wx::ID_ANY);
-        boxleft.add_sizer_int(Some(&clrbox), 0, wx::ALL | wx::GROW, 5, wx::Object::none());
+        // left pane: style
+        let sizer_left = wx::BoxSizer::new(wx::VERTICAL);
 
-        boxleft.add_window_int(
+        let kinds = wx::ArrayString::new();
+        kinds.add("&Default");
+        kinds.add("&Spin");
+        kinds.add("Drop do&wn");
+        let radio_kind = wx::RadioBox::builder(Some(&self.base))
+            .choices(kinds)
+            .major_dimension(1)
+            .style(wx::RA_SPECIFY_COLS.into())
+            .build();
+        sizer_left.add_window_sizerflags(
+            Some(&radio_kind),
+            wx::SizerFlags::new(0).expand().border(wx::ALL),
+        );
+
+        let sizer_style =
+            wx::StaticBoxSizer::new_with_int(wx::VERTICAL, Some(&self.base), "&Style");
+        let chk_style_century =
+            self.create_check_box_and_add_to_sizer(&sizer_style, "Show &century", wx::ID_ANY);
+        let chk_style_allow_none =
+            self.create_check_box_and_add_to_sizer(&sizer_style, "Allow &no value", wx::ID_ANY);
+        sizer_left.add_sizer_sizerflags(
+            Some(&sizer_style),
+            wx::SizerFlags::new(0).expand().border(wx::ALL),
+        );
+        sizer_left.add_window_sizerflags(
             Some(
                 &wx::Button::builder(Some(&self.base))
                     .id(DatePickerPage::Reset.into())
-                    .label("&Reset")
+                    .label("&Recreate")
                     .build(),
             ),
+            wx::SizerFlags::new(0).centre().border(wx::ALL),
+        );
+
+        // middle pane: operations
+        let sizer_middle = wx::BoxSizer::new(wx::VERTICAL);
+        let (sizer_row, text_cur) = self.create_sizer_with_text_and_button(
+            DatePickerPage::Set.into(),
+            "&Set date",
+            wx::ID_ANY,
+        );
+        sizer_middle.add_sizer_sizerflags(
+            Some(&sizer_row),
+            wx::SizerFlags::new(0).expand().border(wx::ALL),
+        );
+
+        text_cur.set_min_size(&wx::Size::new_with_int(
+            self.base.get_text_extent("  9999-99-99  ").get_width(),
+            -1,
+        ));
+
+        sizer_middle.add_spacer(10);
+
+        let (sizer_row, text_min) = self.create_sizer_with_text_and_label("&Min date", wx::ID_ANY);
+        sizer_middle.add_sizer_sizerflags(
+            Some(&sizer_row),
+            wx::SizerFlags::new(0).expand().border(wx::ALL),
+        );
+
+        let (sizer_row, text_max) = self.create_sizer_with_text_and_label("Ma&x date", wx::ID_ANY);
+        sizer_middle.add_sizer_sizerflags(
+            Some(&sizer_row),
+            wx::SizerFlags::new(0).expand().border(wx::ALL),
+        );
+
+        sizer_middle.add_window_sizerflags(
+            Some(
+                &wx::Button::builder(Some(&self.base))
+                    .id(DatePickerPage::SetRange.into())
+                    .label("Set &range")
+                    .build(),
+            ),
+            wx::SizerFlags::new(0).centre().border(wx::ALL),
+        );
+
+        sizer_middle.add_spacer(10);
+
+        let (sizer_row, text_null) =
+            self.create_sizer_with_text_and_label("&Null text", wx::ID_ANY);
+        sizer_middle.add_sizer_sizerflags(
+            Some(&sizer_row),
+            wx::SizerFlags::new(0).expand().border(wx::ALL),
+        );
+
+        sizer_middle.add_window_sizerflags(
+            Some(
+                &wx::Button::builder(Some(&self.base))
+                    .id(DatePickerPage::SetNullText.into())
+                    .label("Set &null text")
+                    .build(),
+            ),
+            wx::SizerFlags::new(0).centre().border(wx::ALL),
+        );
+
+        // right pane: control itself
+        let sizer_right = wx::BoxSizer::new(wx::HORIZONTAL);
+
+        let date_picker = wx::DatePickerCtrl::builder(Some(&self.base))
+            .id(DatePickerPage::Picker.into())
+            .build();
+
+        let centre = wx::CENTRE.try_into().unwrap();
+        sizer_right.add_int_int(0, 0, 1, centre, 0, wx::Object::none());
+        sizer_right.add_window_int(Some(&date_picker), 1, centre, 0, wx::Object::none());
+        sizer_right.add_int_int(0, 0, 1, centre, 0, wx::Object::none());
+
+        // the 3 panes panes compose the window
+        sizer_top.add_sizer_int(
+            Some(&sizer_left),
             0,
-            wx::ALIGN_CENTRE_HORIZONTAL | wx::ALL,
-            15,
+            (wx::ALL & !wx::LEFT),
+            10,
+            wx::Object::none(),
+        );
+        sizer_top.add_sizer_int(
+            Some(&sizer_middle),
+            0,
+            (wx::TOP | wx::BOTTOM),
+            10,
+            wx::Object::none(),
+        );
+        sizer_top.add_sizer_int(
+            Some(&sizer_right),
+            1,
+            wx::GROW | (wx::ALL & !wx::RIGHT),
+            10,
             wx::Object::none(),
         );
 
-        self.reset(); // set checkboxes state
-
-        // create pickers
-        self.create_picker();
-
-        // right pane
-        let sizer = wx::BoxSizer::new(wx::VERTICAL);
-        sizer.add_int_int(1, 1, 1, wx::GROW | wx::ALL, 5, wx::Object::none()); // spacer
-        if let Some(date_picker) = self.date_picker.borrow().as_ref() {
-            sizer.add_window_int(
-                Some(date_picker),
-                0,
-                wx::ALIGN_CENTER | wx::ALL,
-                5,
-                wx::Object::none(),
-            );
-        }
-        sizer.add_int_int(1, 1, 1, wx::GROW | wx::ALL, 5, wx::Object::none()); // spacer
-
-        // global pane
-        let sz = wx::BoxSizer::new(wx::HORIZONTAL);
-        sz.add_sizer_int(Some(&boxleft), 0, wx::GROW | wx::ALL, 5, wx::Object::none());
-        sz.add_sizer_int(Some(&sizer), 1, wx::GROW | wx::ALL, 5, wx::Object::none());
-
         *self.config_ui.borrow_mut() = Some(ConfigUI {
-            chk_colour_text_ctrl,
-            chk_colour_show_label,
-            chk_colour_show_alpha,
-
-            sizer,
+            // chk_colour_text_ctrl,
+            // chk_colour_show_label,
+            // chk_colour_show_alpha,
+            sizer: sizer_right,
         });
 
-        self.base.set_sizer(Some(&sz), true);
+        self.base.set_sizer(Some(&sizer_top), true);
     }
 
     fn handle_button(&self, _: &wx::CommandEvent) {
@@ -161,15 +244,15 @@ impl DatePickerWidgetsPage {
 
     fn reset(&self) {
         if let Some(config_ui) = self.config_ui.borrow().as_ref() {
-            config_ui
-                .chk_colour_text_ctrl
-                .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_USE_TEXTCTRL) != 0);
-            config_ui
-                .chk_colour_show_label
-                .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_SHOW_LABEL) != 0);
-            config_ui
-                .chk_colour_show_alpha
-                .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_SHOW_ALPHA) != 0);
+            // config_ui
+            //     .chk_colour_text_ctrl
+            //     .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_USE_TEXTCTRL) != 0);
+            // config_ui
+            //     .chk_colour_show_label
+            //     .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_SHOW_LABEL) != 0);
+            // config_ui
+            //     .chk_colour_show_alpha
+            //     .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_SHOW_ALPHA) != 0);
         }
     }
 
@@ -180,15 +263,15 @@ impl DatePickerWidgetsPage {
 
         let mut style = wx::BORDER_DEFAULT;
         if let Some(config_ui) = self.config_ui.borrow().as_ref() {
-            if config_ui.chk_colour_text_ctrl.get_value() {
-                style |= wx::CLRP_USE_TEXTCTRL as c_long;
-            }
-            if config_ui.chk_colour_show_label.get_value() {
-                style |= wx::CLRP_SHOW_LABEL as c_long;
-            }
-            if config_ui.chk_colour_show_alpha.get_value() {
-                style |= wx::CLRP_SHOW_ALPHA as c_long;
-            }
+            // if config_ui.chk_colour_text_ctrl.get_value() {
+            //     style |= wx::CLRP_USE_TEXTCTRL as c_long;
+            // }
+            // if config_ui.chk_colour_show_label.get_value() {
+            //     style |= wx::CLRP_SHOW_LABEL as c_long;
+            // }
+            // if config_ui.chk_colour_show_alpha.get_value() {
+            //     style |= wx::CLRP_SHOW_ALPHA as c_long;
+            // }
         }
 
         let date_picker = wx::DatePickerCtrl::builder(Some(&self.base))
