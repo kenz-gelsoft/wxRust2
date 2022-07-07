ae50b80e36e56ae8c64e73824bb3c922c630d956
KENZ <KENZ.gelsoft@gmail.com>
2022-07-08 06:27:47 +0900

    Replace Date to Dir and comment out lines generate compile errors

--- samples/widgets/src/dirpicker.rs
+++ samples/widgets/src/dirpicker.rs
@@ -6,16 +6,16 @@ use wx::methods::*;
 
 // control ids
 #[derive(Clone, Copy)]
-enum DatePickerPage {
+enum DirPickerPage {
     Reset = wx::ID_HIGHEST as isize,
     Set,
     SetRange,
     // SetNullText,
     Picker,
 }
-impl DatePickerPage {
+impl DirPickerPage {
     fn from(v: c_int) -> Option<Self> {
-        use DatePickerPage::*;
+        use DirPickerPage::*;
         for e in [
             Reset, Set, SetRange, // SetNullText,
             Picker,
@@ -27,8 +27,8 @@ impl DatePickerPage {
         return None;
     }
 }
-impl From<DatePickerPage> for c_int {
-    fn from(w: DatePickerPage) -> Self {
+impl From<DirPickerPage> for c_int {
+    fn from(w: DirPickerPage) -> Self {
         w as c_int
     }
 }
@@ -37,7 +37,7 @@ impl From<DatePickerPage> for c_int {
 pub struct ConfigUI {
     // other controls
     // --------------
-    sizer_date_picker: wx::BoxSizer,
+    sizer_dir_picker: wx::BoxSizer,
 
     text_cur: wx::TextCtrl,
     text_min: wx::TextCtrl,
@@ -49,18 +49,18 @@ pub struct ConfigUI {
 }
 
 #[derive(Clone)]
-pub struct DatePickerWidgetsPage {
+pub struct DirPickerWidgetsPage {
     pub base: wx::Panel,
     config_ui: RefCell<Option<ConfigUI>>,
     // the picker
-    date_picker: Rc<RefCell<Option<wx::DatePickerCtrl>>>,
+    dir_picker: Rc<RefCell<Option<wx::DirPickerCtrl>>>,
 }
-impl WidgetsPage for DatePickerWidgetsPage {
+impl WidgetsPage for DirPickerWidgetsPage {
     fn base(&self) -> &wx::Panel {
         return &self.base;
     }
     fn label(&self) -> &str {
-        return "DatePicker";
+        return "DirPicker";
     }
     fn create_content(&self) {
         let sizer_top = wx::BoxSizer::new(wx::HORIZONTAL);
@@ -95,7 +95,7 @@ impl WidgetsPage for DatePickerWidgetsPage {
         sizer_left.add_window_sizerflags(
             Some(
                 &wx::Button::builder(Some(&self.base))
-                    .id(DatePickerPage::Reset.into())
+                    .id(DirPickerPage::Reset.into())
                     .label("&Recreate")
                     .build(),
             ),
@@ -105,8 +105,8 @@ impl WidgetsPage for DatePickerWidgetsPage {
         // middle pane: operations
         let sizer_middle = wx::BoxSizer::new(wx::VERTICAL);
         let (sizer_row, text_cur) = self.create_sizer_with_text_and_button(
-            DatePickerPage::Set.into(),
-            "&Set date",
+            DirPickerPage::Set.into(),
+            "&Set dir",
             wx::ID_ANY,
         );
         sizer_middle.add_sizer_sizerflags(
@@ -121,13 +121,13 @@ impl WidgetsPage for DatePickerWidgetsPage {
 
         sizer_middle.add_spacer(10);
 
-        let (sizer_row, text_min) = self.create_sizer_with_text_and_label("&Min date", wx::ID_ANY);
+        let (sizer_row, text_min) = self.create_sizer_with_text_and_label("&Min dir", wx::ID_ANY);
         sizer_middle.add_sizer_sizerflags(
             Some(&sizer_row),
             wx::SizerFlags::new(0).expand().border(wx::ALL),
         );
 
-        let (sizer_row, text_max) = self.create_sizer_with_text_and_label("Ma&x date", wx::ID_ANY);
+        let (sizer_row, text_max) = self.create_sizer_with_text_and_label("Ma&x dir", wx::ID_ANY);
         sizer_middle.add_sizer_sizerflags(
             Some(&sizer_row),
             wx::SizerFlags::new(0).expand().border(wx::ALL),
@@ -136,7 +136,7 @@ impl WidgetsPage for DatePickerWidgetsPage {
         sizer_middle.add_window_sizerflags(
             Some(
                 &wx::Button::builder(Some(&self.base))
-                    .id(DatePickerPage::SetRange.into())
+                    .id(DirPickerPage::SetRange.into())
                     .label("Set &range")
                     .build(),
             ),
@@ -155,7 +155,7 @@ impl WidgetsPage for DatePickerWidgetsPage {
         // sizer_middle.add_window_sizerflags(
         //     Some(
         //         &wx::Button::builder(Some(&self.base))
-        //             .id(DatePickerPage::SetNullText.into())
+        //             .id(DirPickerPage::SetNullText.into())
         //             .label("Set &null text")
         //             .build(),
         //     ),
@@ -165,15 +165,15 @@ impl WidgetsPage for DatePickerWidgetsPage {
         // right pane: control itself
         let sizer_right = wx::BoxSizer::new(wx::HORIZONTAL);
 
-        let date_picker = wx::DatePickerCtrl::builder(Some(&self.base))
-            .id(DatePickerPage::Picker.into())
+        let dir_picker = wx::DirPickerCtrl::builder(Some(&self.base))
+            .id(DirPickerPage::Picker.into())
             .build();
 
         let centre = wx::CENTRE.try_into().unwrap();
         sizer_right.add_int_int(0, 0, 1, centre, 0, wx::Object::none());
-        sizer_right.add_window_int(Some(&date_picker), 1, centre, 0, wx::Object::none());
+        sizer_right.add_window_int(Some(&dir_picker), 1, centre, 0, wx::Object::none());
         sizer_right.add_int_int(0, 0, 1, centre, 0, wx::Object::none());
-        *self.date_picker.borrow_mut() = Some(date_picker);
+        *self.dir_picker.borrow_mut() = Some(dir_picker);
 
         // the 3 panes panes compose the window
         sizer_top.add_sizer_int(
@@ -202,7 +202,7 @@ impl WidgetsPage for DatePickerWidgetsPage {
         chk_style_century.set_value(true);
 
         let config_ui = ConfigUI {
-            sizer_date_picker: sizer_right, // save it to modify it later
+            sizer_dir_picker: sizer_right, // save it to modify it later
 
             text_cur,
             text_min,
@@ -222,13 +222,13 @@ impl WidgetsPage for DatePickerWidgetsPage {
         println!("event={}", event.get_id());
         if let (Some(config_ui), Some(m)) = (
             self.config_ui.borrow().as_ref(),
-            DatePickerPage::from(event.get_id()),
+            DirPickerPage::from(event.get_id()),
         ) {
             match m {
-                DatePickerPage::Reset => self.on_button_reset(config_ui),
-                DatePickerPage::Set => self.on_button_set(config_ui),
-                DatePickerPage::SetRange => self.on_button_set_range(config_ui),
-                // DatePickerPage::SetNullText => self.on_button_set_null_text(config_ui),
+                DirPickerPage::Reset => self.on_button_reset(config_ui),
+                DirPickerPage::Set => self.on_button_set(config_ui),
+                DirPickerPage::SetRange => self.on_button_set_range(config_ui),
+                // DirPickerPage::SetNullText => self.on_button_set_null_text(config_ui),
                 _ => (),
             };
         }
@@ -240,44 +240,44 @@ impl WidgetsPage for DatePickerWidgetsPage {
         // Do nothing
     }
 }
-impl DatePickerWidgetsPage {
+impl DirPickerWidgetsPage {
     pub fn new<P: WindowMethods>(book: &P) -> Self {
         let panel = wx::Panel::builder(Some(book))
             .style(wx::CLIP_CHILDREN | wx::TAB_TRAVERSAL)
             .build();
-        DatePickerWidgetsPage {
+        DirPickerWidgetsPage {
             base: panel,
             config_ui: RefCell::new(None),
-            date_picker: Rc::new(RefCell::new(None)),
+            dir_picker: Rc::new(RefCell::new(None)),
         }
     }
 
     fn recreate_widget(&self) {
         if let Some(config_ui) = self.config_ui.borrow().as_ref() {
-            self.create_date_picker(config_ui);
+            self.create_dir_picker(config_ui);
         }
     }
 
     fn reset(&self, config_ui: &ConfigUI) {
-        let today = wx::DateTime::today();
+        // let today = wx::DirTime::today();
 
-        if let Some(date_picker) = self.date_picker.borrow().as_ref() {
-            date_picker.set_value(&today);
-        }
-        config_ui.text_cur.set_value(&today.format_iso_date());
+        // if let Some(dir_picker) = self.dir_picker.borrow().as_ref() {
+        //     dir_picker.set_value(&today);
+        // }
+        // config_ui.text_cur.set_value(&today.format_iso_dir());
     }
 
-    fn create_date_picker(&self, config_ui: &ConfigUI) {
-        let mut value: Option<wx::DateTime> = None;
-        if let Some(date_picker) = self.date_picker.borrow().as_ref() {
-            value = Some(date_picker.get_value());
+    fn create_dir_picker(&self, config_ui: &ConfigUI) {
+        // let mut value: Option<wx::DirTime> = None;
+        if let Some(dir_picker) = self.dir_picker.borrow().as_ref() {
+            // value = Some(dir_picker.get_value());
 
             // TODO: remove (and delete) all buttons
-            let count = config_ui.sizer_date_picker.get_children().get_count();
+            let count = config_ui.sizer_dir_picker.get_children().get_count();
             for _ in 0..count {
-                config_ui.sizer_date_picker.remove_int(0);
+                config_ui.sizer_dir_picker.remove_int(0);
             }
-            date_picker.destroy();
+            dir_picker.destroy();
         }
 
         let mut style = wx::BORDER_DEFAULT;
@@ -295,90 +295,90 @@ impl DatePickerWidgetsPage {
             style |= wx::DP_ALLOWNONE as c_long;
         }
 
-        let date_picker = wx::DatePickerCtrl::builder(Some(&self.base))
-            .id(DatePickerPage::Picker.into())
+        let dir_picker = wx::DirPickerCtrl::builder(Some(&self.base))
+            .id(DirPickerPage::Picker.into())
             .style(style)
-            .dt(value)
+            // .dt(value)
             .build();
 
         let centre = wx::CENTRE.try_into().unwrap();
         config_ui
-            .sizer_date_picker
+            .sizer_dir_picker
             .add_int_int(0, 0, 1, centre, 0, wx::Object::none());
-        config_ui.sizer_date_picker.add_window_int(
-            Some(&date_picker),
+        config_ui.sizer_dir_picker.add_window_int(
+            Some(&dir_picker),
             1,
             centre,
             0,
             wx::Object::none(),
         );
         config_ui
-            .sizer_date_picker
+            .sizer_dir_picker
             .add_int_int(0, 0, 1, centre, 0, wx::Object::none());
-        config_ui.sizer_date_picker.layout();
-        *self.date_picker.borrow_mut() = Some(date_picker);
+        config_ui.sizer_dir_picker.layout();
+        *self.dir_picker.borrow_mut() = Some(dir_picker);
     }
 
     fn on_button_reset(&self, config_ui: &ConfigUI) {
         self.reset(config_ui);
-        self.create_date_picker(config_ui);
+        self.create_dir_picker(config_ui);
     }
 
-    fn get_date_from_text_control(&self, text: &wx::TextCtrl) -> Option<wx::DateTime> {
-        let value = text.get_value();
-        if !value.is_empty() {
-            let dt = wx::DateTime::new();
-            if let Some(len) = dt.parse_date(&value) {
-                if len == value.len() {
-                    return Some(dt);
-                }
-            }
-            println!("Invalid date \"{}\"", value);
-        }
-        return None;
-    }
+    // fn get_dir_from_text_control(&self, text: &wx::TextCtrl) -> Option<wx::DirTime> {
+    //     let value = text.get_value();
+    //     if !value.is_empty() {
+    //         let dt = wx::DirTime::new();
+    //         if let Some(len) = dt.parse_dir(&value) {
+    //             if len == value.len() {
+    //                 return Some(dt);
+    //             }
+    //         }
+    //         println!("Invalid dir \"{}\"", value);
+    //     }
+    //     return None;
+    // }
 
     fn on_button_set(&self, config_ui: &ConfigUI) {
-        if let (Some(dt), Some(date_picker)) = (
-            self.get_date_from_text_control(&config_ui.text_cur),
-            self.date_picker.borrow().as_ref(),
-        ) {
-            date_picker.set_value(&dt);
-        }
+        // if let (Some(dt), Some(dir_picker)) = (
+        //     self.get_dir_from_text_control(&config_ui.text_cur),
+        //     self.dir_picker.borrow().as_ref(),
+        // ) {
+        //     dir_picker.set_value(&dt);
+        // }
     }
 
     fn on_button_set_range(&self, config_ui: &ConfigUI) {
-        if let (Some(dt1), Some(dt2), Some(date_picker)) = (
-            self.get_date_from_text_control(&config_ui.text_min),
-            self.get_date_from_text_control(&config_ui.text_max),
-            self.date_picker.borrow().as_ref(),
-        ) {
-            date_picker.set_range(&dt1, &dt2);
+        // if let (Some(dt1), Some(dt2), Some(dir_picker)) = (
+        //     self.get_dir_from_text_control(&config_ui.text_min),
+        //     self.get_dir_from_text_control(&config_ui.text_max),
+        //     self.dir_picker.borrow().as_ref(),
+        // ) {
+        //     dir_picker.set_range(&dt1, &dt2);
 
-            if !date_picker.get_range(Some(&dt1), Some(&dt2)) {
-                println!("No range set");
-            } else {
-                let dt1 = if dt1.is_valid() {
-                    dt1.format_iso_date()
-                } else {
-                    String::new()
-                };
-                config_ui.text_min.set_value(&dt1);
-                let dt2 = if dt2.is_valid() {
-                    dt2.format_iso_date()
-                } else {
-                    String::new()
-                };
-                config_ui.text_max.set_value(&dt2);
+        //     if !dir_picker.get_range(Some(&dt1), Some(&dt2)) {
+        //         println!("No range set");
+        //     } else {
+        //         let dt1 = if dt1.is_valid() {
+        //             dt1.format_iso_dir()
+        //         } else {
+        //             String::new()
+        //         };
+        //         config_ui.text_min.set_value(&dt1);
+        //         let dt2 = if dt2.is_valid() {
+        //             dt2.format_iso_dir()
+        //         } else {
+        //             String::new()
+        //         };
+        //         config_ui.text_max.set_value(&dt2);
 
-                println!("Date picker range updated");
-            }
-        }
+        //         println!("Dir picker range updird");
+        //     }
+        // }
     }
 
     // fn on_button_set_null_text(&self, config_ui: &ConfigUI) {
-    //     if let Some(date_picker) = self.date_picker.borrow().as_ref() {
-    //         date_picker.set_null_text(&config_ui.text_null.get_value());
+    //     if let Some(dir_picker) = self.dir_picker.borrow().as_ref() {
+    //         dir_picker.set_null_text(&config_ui.text_null.get_value());
     //     }
     // }
 }
