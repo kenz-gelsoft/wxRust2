3541e0cbc4da989421f568192731b14681f390a9
KENZ <KENZ.gelsoft@gmail.com>
2022-07-17 03:40:01 +0900

    widgets: Impl create_content() for FontPickerCtrl page

--- samples/widgets/src/fontpicker.rs
+++ samples/widgets/src/fontpicker.rs
@@ -8,12 +8,12 @@ use wx::methods::*;
 #[derive(Clone, Copy)]
 enum PickerPage {
     Reset = wx::ID_HIGHEST as isize,
-    Colour,
+    Font,
 }
 impl PickerPage {
     fn from(v: c_int) -> Option<Self> {
         use PickerPage::*;
-        for e in [Reset, Colour] {
+        for e in [Reset, Font] {
             if v == e.into() {
                 return Some(e);
             }
@@ -31,39 +31,39 @@ impl From<PickerPage> for c_int {
 pub struct ConfigUI {
     // other controls
     // --------------
-    chk_colour_text_ctrl: wx::CheckBox,
-    chk_colour_show_label: wx::CheckBox,
-    chk_colour_show_alpha: wx::CheckBox,
+    chk_font_text_ctrl: wx::CheckBox,
+    chk_font_desc_as_label: wx::CheckBox,
+    chk_font_use_font_for_label: wx::CheckBox,
     sizer: wx::BoxSizer,
 }
 
 #[derive(Clone)]
-pub struct ColourPickerWidgetsPage {
+pub struct FontPickerWidgetsPage {
     pub base: wx::Panel,
     config_ui: RefCell<Option<ConfigUI>>,
     // the picker
-    clr_picker: Rc<RefCell<Option<wx::ColourPickerCtrl>>>,
+    font_picker: Rc<RefCell<Option<wx::FontPickerCtrl>>>,
 }
-impl WidgetsPage for ColourPickerWidgetsPage {
+impl WidgetsPage for FontPickerWidgetsPage {
     fn base(&self) -> &wx::Panel {
         return &self.base;
     }
     fn label(&self) -> &str {
-        return "ColourPicker";
+        return "FontPicker";
     }
     fn create_content(&self) {
         // left pane
         let boxleft = wx::BoxSizer::new(wx::VERTICAL);
 
-        let clrbox =
-            wx::StaticBoxSizer::new_with_int(wx::VERTICAL, Some(&self.base), "&ColourPicker style");
-        let chk_colour_text_ctrl =
-            self.create_check_box_and_add_to_sizer(&clrbox, "With label", wx::ID_ANY);
-        let chk_colour_show_label =
-            self.create_check_box_and_add_to_sizer(&clrbox, "With textctrl", wx::ID_ANY);
-        let chk_colour_show_alpha =
-            self.create_check_box_and_add_to_sizer(&clrbox, "With opacity", wx::ID_ANY);
-        boxleft.add_sizer_int(Some(&clrbox), 0, wx::ALL | wx::GROW, 5, wx::Object::none());
+        let fontbox =
+            wx::StaticBoxSizer::new_with_int(wx::VERTICAL, Some(&self.base), "&FontPicker style");
+        let chk_font_text_ctrl  =
+            self.create_check_box_and_add_to_sizer(&fontbox, "With textctrl", wx::ID_ANY);
+        let chk_font_desc_as_label  =
+            self.create_check_box_and_add_to_sizer(&fontbox, "Font desc as btn label", wx::ID_ANY);
+        let chk_font_use_font_for_label  =
+            self.create_check_box_and_add_to_sizer(&fontbox, "Use font for label", wx::ID_ANY);
+        boxleft.add_sizer_int(Some(&fontbox), 0, wx::ALL | wx::GROW, 5, wx::Object::none());
 
         boxleft.add_window_int(
             Some(
@@ -86,9 +86,9 @@ impl WidgetsPage for ColourPickerWidgetsPage {
         // right pane
         let sizer = wx::BoxSizer::new(wx::VERTICAL);
         sizer.add_int_int(1, 1, 1, wx::GROW | wx::ALL, 5, wx::Object::none()); // spacer
-        if let Some(clr_picker) = self.clr_picker.borrow().as_ref() {
+        if let Some(font_picker) = self.font_picker.borrow().as_ref() {
             sizer.add_window_int(
-                Some(clr_picker),
+                Some(font_picker),
                 0,
                 wx::ALIGN_CENTER | wx::ALL,
                 5,
@@ -103,9 +103,9 @@ impl WidgetsPage for ColourPickerWidgetsPage {
         sz.add_sizer_int(Some(&sizer), 1, wx::GROW | wx::ALL, 5, wx::Object::none());
 
         *self.config_ui.borrow_mut() = Some(ConfigUI {
-            chk_colour_text_ctrl,
-            chk_colour_show_label,
-            chk_colour_show_alpha,
+            chk_font_text_ctrl,
+            chk_font_desc_as_label,
+            chk_font_use_font_for_label,
 
             sizer,
         });
@@ -123,15 +123,15 @@ impl WidgetsPage for ColourPickerWidgetsPage {
         // Do nothing
     }
 }
-impl ColourPickerWidgetsPage {
+impl FontPickerWidgetsPage {
     pub fn new<P: WindowMethods>(book: &P) -> Self {
         let panel = wx::Panel::builder(Some(book))
             .style(wx::CLIP_CHILDREN | wx::TAB_TRAVERSAL)
             .build();
-        ColourPickerWidgetsPage {
+        FontPickerWidgetsPage {
             base: panel,
             config_ui: RefCell::new(None),
-            clr_picker: Rc::new(RefCell::new(None)),
+            font_picker: Rc::new(RefCell::new(None)),
         }
     }
 
@@ -142,10 +142,10 @@ impl ColourPickerWidgetsPage {
             // MEMO: Destroy()ing in create_picker() removes from its sizer.
             // config_ui.sizer.remove_int(1);
             self.create_picker();
-            if let Some(clr_pickr) = self.clr_picker.borrow().as_ref() {
+            if let Some(font_pickr) = self.font_picker.borrow().as_ref() {
                 config_ui.sizer.insert_window_int(
                     1,
-                    Some(clr_pickr),
+                    Some(font_pickr),
                     0,
                     wx::ALIGN_CENTER | wx::ALL,
                     5,
@@ -158,42 +158,42 @@ impl ColourPickerWidgetsPage {
 
     fn reset(&self) {
         if let Some(config_ui) = self.config_ui.borrow().as_ref() {
-            config_ui
-                .chk_colour_text_ctrl
-                .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_USE_TEXTCTRL) != 0);
-            config_ui
-                .chk_colour_show_label
-                .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_SHOW_LABEL) != 0);
-            config_ui
-                .chk_colour_show_alpha
-                .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_SHOW_ALPHA) != 0);
+            // config_ui
+            //     .chk_font_text_ctrl
+            //     .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_USE_TEXTCTRL) != 0);
+            // config_ui
+            //     .chk_font_show_label
+            //     .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_SHOW_LABEL) != 0);
+            // config_ui
+            //     .chk_font_show_alpha
+            //     .set_value((wx::CLRP_DEFAULT_STYLE & wx::CLRP_SHOW_ALPHA) != 0);
         }
     }
 
     fn create_picker(&self) {
-        if let Some(clr_picker) = self.clr_picker.borrow().as_ref() {
-            clr_picker.destroy();
+        if let Some(font_picker) = self.font_picker.borrow().as_ref() {
+            font_picker.destroy();
         }
 
         let mut style = wx::BORDER_DEFAULT;
         if let Some(config_ui) = self.config_ui.borrow().as_ref() {
-            if config_ui.chk_colour_text_ctrl.get_value() {
-                style |= wx::CLRP_USE_TEXTCTRL as c_long;
-            }
-            if config_ui.chk_colour_show_label.get_value() {
-                style |= wx::CLRP_SHOW_LABEL as c_long;
-            }
-            if config_ui.chk_colour_show_alpha.get_value() {
-                style |= wx::CLRP_SHOW_ALPHA as c_long;
-            }
+            // if config_ui.chk_font_text_ctrl.get_value() {
+            //     style |= wx::CLRP_USE_TEXTCTRL as c_long;
+            // }
+            // if config_ui.chk_font_show_label.get_value() {
+            //     style |= wx::CLRP_SHOW_LABEL as c_long;
+            // }
+            // if config_ui.chk_font_show_alpha.get_value() {
+            //     style |= wx::CLRP_SHOW_ALPHA as c_long;
+            // }
         }
 
-        let clr_picker = wx::ColourPickerCtrl::builder(Some(&self.base))
-            .id(PickerPage::Colour.into())
-            .colour(wx::Colour::new_with_str("RED"))
+        let font_picker = wx::FontPickerCtrl::builder(Some(&self.base))
+            .id(PickerPage::Font.into())
+            // .font(wx::Font::new_with_str("RED"))
             .style(style)
             .build();
-        *self.clr_picker.borrow_mut() = Some(clr_picker);
+        *self.font_picker.borrow_mut() = Some(font_picker);
     }
 
     fn on_button_reset(&self) {
