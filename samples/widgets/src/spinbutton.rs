1582593bffd92efe98a62361f165db7723b6788d
KENZ <KENZ.gelsoft@gmail.com>
2022-08-06 12:40:48 +0900

    widgets: Impl create_content()/_spin() for Spin page

--- samples/widgets/src/spinbutton.rs
+++ samples/widgets/src/spinbutton.rs
@@ -6,15 +6,43 @@ use wx::methods::*;
 
 // control ids
 #[derive(Clone, Copy)]
-enum EditableListboxPage {
+enum SpinBtnPage {
     Reset = wx::ID_HIGHEST as isize,
-    Listbox,
-    ContainerTests,
+    Clear,
+    SetValue,
+    SetMinAndMax,
+    SetBase,
+    SetIncrement,
+    CurValueText,
+    ValueText,
+    MinText,
+    MaxText,
+    BaseText,
+    SetIncrementText,
+    SpinBtn,
+    SpinCtrl,
+    SpinCtrlDouble,
 }
-impl EditableListboxPage {
+impl SpinBtnPage {
     fn from(v: c_int) -> Option<Self> {
-        use EditableListboxPage::*;
-        for e in [Reset, Listbox, ContainerTests] {
+        use SpinBtnPage::*;
+        for e in [
+            Reset,
+            Clear,
+            SetValue,
+            SetMinAndMax,
+            SetBase,
+            SetIncrement,
+            CurValueText,
+            ValueText,
+            MinText,
+            MaxText,
+            BaseText,
+            SetIncrementText,
+            SpinBtn,
+            SpinCtrl,
+            SpinCtrlDouble,
+        ] {
             if v == e.into() {
                 return Some(e);
             }
@@ -22,8 +50,8 @@ impl EditableListboxPage {
         return None;
     }
 }
-impl From<EditableListboxPage> for c_int {
-    fn from(w: EditableListboxPage) -> Self {
+impl From<SpinBtnPage> for c_int {
+    fn from(w: SpinBtnPage) -> Self {
         w as c_int
     }
 }
@@ -31,12 +59,12 @@ impl From<EditableListboxPage> for c_int {
 #[derive(Clone)]
 pub struct ConfigUI {
     // the checkboxes
-    chk_allow_new: wx::CheckBox,
-    chk_allow_edit: wx::CheckBox,
-    chk_allow_delete: wx::CheckBox,
-    chk_allow_no_reorder: wx::CheckBox,
+    chk_vert: wx::CheckBox,
+    chk_arrow_keys: wx::CheckBox,
+    chk_wrap: wx::CheckBox,
+    chk_process_enter: wx::CheckBox,
 
-    sizer_lbox: wx::BoxSizer,
+    sizer_spin: wx::BoxSizer,
 }
 
 #[derive(Clone)]
@@ -44,7 +72,12 @@ pub struct SpinButtonWidgetsPage {
     pub base: wx::Panel,
     config_ui: RefCell<Option<ConfigUI>>,
     // the control itself
-    lbox: Rc<RefCell<Option<wx::EditableListBox>>>,
+    spinbtn: Rc<RefCell<Option<wx::SpinButton>>>,
+    spinctrl: Rc<RefCell<Option<wx::SpinCtrl>>>,
+    spinctrldbl: Rc<RefCell<Option<wx::SpinCtrlDouble>>>,
+
+    min: RefCell<c_int>,
+    max: RefCell<c_int>,
 }
 impl WidgetsPage for SpinButtonWidgetsPage {
     fn base(&self) -> &wx::Panel {
@@ -54,32 +87,45 @@ impl WidgetsPage for SpinButtonWidgetsPage {
         return "Spin";
     }
     fn create_content(&self) {
-        /*
-        What we create here is a frame having 2 panes: style pane is the
-        leftmost one and the pane containing the listbox itself to the right
-        */
         let sizer_top = wx::BoxSizer::new(wx::HORIZONTAL);
 
         // left pane
-        let box_left = wx::StaticBox::builder(Some(&self.base))
-            .label("&Set listbox parameters")
+        let s_box = wx::StaticBox::builder(Some(&self.base))
+            .label("&Set style")
             .build();
-        let sizer_left = wx::StaticBoxSizer::new_with_staticbox(Some(&box_left), wx::VERTICAL);
+        let sizer_left = wx::StaticBoxSizer::new_with_staticbox(Some(&s_box), wx::VERTICAL);
 
-        let chk_allow_new =
-            self.create_check_box_and_add_to_sizer(&sizer_left, "Allow new items", wx::ID_ANY);
-        let chk_allow_edit =
-            self.create_check_box_and_add_to_sizer(&sizer_left, "Allow editing items", wx::ID_ANY);
-        let chk_allow_delete =
-            self.create_check_box_and_add_to_sizer(&sizer_left, "Allow deleting items", wx::ID_ANY);
-        let chk_allow_no_reorder = self.create_check_box_and_add_to_sizer(
-            &sizer_left,
-            "Block user reordering",
-            wx::ID_ANY,
+        let chk_vert = self.create_check_box_and_add_to_sizer(&sizer_left, "&Vertical", wx::ID_ANY);
+        let chk_arrow_keys =
+            self.create_check_box_and_add_to_sizer(&sizer_left, "&Arrow Keys", wx::ID_ANY);
+        let chk_wrap = self.create_check_box_and_add_to_sizer(&sizer_left, "&Wrap", wx::ID_ANY);
+        let chk_process_enter =
+            self.create_check_box_and_add_to_sizer(&sizer_left, "Process &Enter", wx::ID_ANY);
+
+        sizer_left.add_int_int(5, 5, 0, wx::GROW | wx::ALL, 5, wx::Object::none()); // spacer
+
+        let halign = wx::ArrayString::new();
+        halign.add("left");
+        halign.add("centre");
+        halign.add("right");
+
+        let radio_align = wx::RadioBox::builder(Some(&self.base))
+            .label("&Text alignment")
+            .choices(halign)
+            .major_dimension(1)
+            .build();
+        sizer_left.add_window_int(
+            Some(&radio_align),
+            0,
+            wx::GROW | wx::ALL,
+            5,
+            wx::Object::none(),
         );
 
+        sizer_left.add_int_int(5, 5, 0, wx::GROW | wx::ALL, 5, wx::Object::none()); // spacer
+
         let btn = wx::Button::builder(Some(&self.base))
-            .id(EditableListboxPage::Reset.into())
+            .id(SpinBtnPage::Reset.into())
             .label("&Reset")
             .build();
         sizer_left.add_window_int(
@@ -90,21 +136,106 @@ impl WidgetsPage for SpinButtonWidgetsPage {
             wx::Object::none(),
         );
 
+        // middle pane
+        let s_box2 = wx::StaticBox::builder(Some(&self.base))
+            .label("&Change spinbtn value")
+            .build();
+        let sizer_middle = wx::StaticBoxSizer::new_with_staticbox(Some(&s_box2), wx::VERTICAL);
+
+        let (sizer_row, text) = self
+            .create_sizer_with_text_and_label("Current value", SpinBtnPage::CurValueText.into());
+        text.set_editable(false);
+        sizer_middle.add_sizer_int(
+            Some(&sizer_row),
+            0,
+            wx::ALL | wx::GROW,
+            5,
+            wx::Object::none(),
+        );
+
+        let (sizer_row, text_value) = self.create_sizer_with_text_and_button(
+            SpinBtnPage::SetValue.into(),
+            "Set &value",
+            SpinBtnPage::ValueText.into(),
+        );
+        sizer_middle.add_sizer_int(
+            Some(&sizer_row),
+            0,
+            wx::ALL | wx::GROW,
+            5,
+            wx::Object::none(),
+        );
+
+        let (sizer_row, text_min) = self.create_sizer_with_text_and_button(
+            SpinBtnPage::SetMinAndMax.into(),
+            "&Min and max",
+            SpinBtnPage::MinText.into(),
+        );
+        let text_max = wx::TextCtrl::builder(Some(&self.base))
+            .id(SpinBtnPage::MaxText.into())
+            .build();
+        sizer_row.add_window_int(
+            Some(&text_max),
+            1,
+            wx::LEFT | wx::ALIGN_CENTRE_VERTICAL,
+            5,
+            wx::Object::none(),
+        );
+        let (min, max) = (*self.min.borrow(), *self.max.borrow());
+        text_min.set_value(&format!("{}", min));
+        text_max.set_value(&format!("{}", max));
+        sizer_middle.add_sizer_int(
+            Some(&sizer_row),
+            0,
+            wx::ALL | wx::GROW,
+            5,
+            wx::Object::none(),
+        );
+
+        let (sizer_row, text_base) = self.create_sizer_with_text_and_button(
+            SpinBtnPage::SetBase.into(),
+            "Set &base",
+            SpinBtnPage::BaseText.into(),
+        );
+        text_base.set_value("10");
+        sizer_middle.add_sizer_int(
+            Some(&sizer_row),
+            0,
+            wx::ALL | wx::GROW,
+            5,
+            wx::Object::none(),
+        );
+
+        let (sizer_row, text_increment) = self.create_sizer_with_text_and_button(
+            SpinBtnPage::SetIncrement.into(),
+            "Set Increment",
+            SpinBtnPage::SetIncrementText.into(),
+        );
+        text_increment.set_value("1");
+        sizer_middle.add_sizer_int(
+            Some(&sizer_row),
+            0,
+            wx::ALL | wx::GROW,
+            5,
+            wx::Object::none(),
+        );
+
         // right pane
         let sizer_right = wx::BoxSizer::new(wx::VERTICAL);
-        let lbox = wx::EditableListBox::builder(Some(&self.base))
-            .id(EditableListboxPage::Listbox.into())
-            .style(0)
-            .build();
-        sizer_right.add_window_int(Some(&lbox), 1, wx::GROW | wx::ALL, 5, wx::Object::none());
         sizer_right.set_min_size_int(150, 0);
-        *self.lbox.borrow_mut() = Some(lbox);
 
         // the 3 panes panes compose the window
         sizer_top.add_sizer_int(
             Some(&sizer_left),
             0,
             wx::GROW | (wx::ALL & !wx::LEFT),
+            10,
+            wx::Object::none(),
+        );
+        sizer_top.add_sizer_int(
+            Some(&sizer_middle),
+            1,
+            wx::GROW | wx::ALL,
             10,
             wx::Object::none(),
         );
@@ -116,15 +247,17 @@ impl WidgetsPage for SpinButtonWidgetsPage {
             wx::Object::none(),
         );
 
-        // final initializations
         let config_ui = ConfigUI {
-            chk_allow_new,
-            chk_allow_edit,
-            chk_allow_delete,
-            chk_allow_no_reorder,
+            chk_vert,
+            chk_arrow_keys,
+            chk_wrap,
+            chk_process_enter,
 
-            sizer_lbox: sizer_right, // save it to modify it later
+            sizer_spin: sizer_right, // save it to modify it later
         };
+        self.create_spin(&config_ui);
+
+        // final initializations
         self.reset(&config_ui);
         *self.config_ui.borrow_mut() = Some(config_ui);
 
@@ -135,10 +268,10 @@ impl WidgetsPage for SpinButtonWidgetsPage {
         println!("event={}", event.get_id());
         if let (Some(config_ui), Some(m)) = (
             self.config_ui.borrow().as_ref(),
-            EditableListboxPage::from(event.get_id()),
+            SpinBtnPage::from(event.get_id()),
         ) {
             match m {
-                EditableListboxPage::Reset => self.on_button_reset(config_ui),
+                SpinBtnPage::Reset => self.on_button_reset(config_ui),
                 _ => (),
             };
         }
@@ -160,58 +293,153 @@ impl SpinButtonWidgetsPage {
         SpinButtonWidgetsPage {
             base: panel,
             config_ui: RefCell::new(None),
-            lbox: Rc::new(RefCell::new(None)),
+            spinbtn: Rc::new(RefCell::new(None)),
+            spinctrl: Rc::new(RefCell::new(None)),
+            spinctrldbl: Rc::new(RefCell::new(None)),
+            min: RefCell::new(0),
+            max: RefCell::new(0),
         }
     }
 
     fn reset(&self, config_ui: &ConfigUI) {
-        config_ui.chk_allow_new.set_value(false);
-        config_ui.chk_allow_edit.set_value(false);
-        config_ui.chk_allow_delete.set_value(false);
-        config_ui.chk_allow_no_reorder.set_value(false);
+        config_ui.chk_vert.set_value(false);
+        config_ui.chk_arrow_keys.set_value(false);
+        config_ui.chk_wrap.set_value(false);
+        config_ui.chk_process_enter.set_value(false);
     }
 
-    fn create_lbox(&self, config_ui: &ConfigUI) {
+    fn create_spin(&self, config_ui: &ConfigUI) {
         let mut flags = wx::BORDER_DEFAULT;
 
-        if config_ui.chk_allow_new.get_value() {
-            flags |= wx::EL_ALLOW_NEW as c_long;
+        flags |= if config_ui.chk_vert.get_value() {
+            wx::SP_VERTICAL
+        } else {
+            wx::SP_HORIZONTAL
+        } as c_long;
+
+        if config_ui.chk_arrow_keys.get_value() {
+            flags |= wx::SP_ARROW_KEYS as c_long;
         }
-        if config_ui.chk_allow_edit.get_value() {
-            flags |= wx::EL_ALLOW_EDIT as c_long;
+        if config_ui.chk_wrap.get_value() {
+            flags |= wx::SP_WRAP as c_long;
         }
-        if config_ui.chk_allow_delete.get_value() {
-            flags |= wx::EL_ALLOW_DELETE as c_long;
-        }
-        if config_ui.chk_allow_no_reorder.get_value() {
-            flags |= wx::EL_NO_REORDER as c_long;
+        if config_ui.chk_process_enter.get_value() {
+            flags |= wx::TE_PROCESS_ENTER as c_long;
         }
 
-        let items = wx::ArrayString::new();
-        if let Some(lbox) = self.lbox.borrow().as_ref() {
-            lbox.get_strings(&items);
-            config_ui.sizer_lbox.detach_window(Some(lbox));
-            lbox.destroy();
+        // TODO: textFlags
+        let mut val = *self.min.borrow();
+        if let Some(spinbtn) = self.spinbtn.borrow().as_ref() {
+            let val_old = spinbtn.get_value();
+            if self.is_valid_value(val_old) {
+                val = val_old;
+            }
+            config_ui.sizer_spin.clear(true);
         }
 
-        let lbox = wx::EditableListBox::builder(Some(&self.base))
-            .id(EditableListboxPage::Listbox.into())
-            .label("Match these wildcards:")
+        let spinbtn = wx::SpinButton::builder(Some(&self.base))
+            .id(SpinBtnPage::SpinBtn.into())
             .style(flags)
             .build();
-        lbox.set_strings(&items);
+        spinbtn.set_value(val);
+        let (min, max) = (*self.min.borrow(), *self.max.borrow());
+        spinbtn.set_range(min, max);
 
-        config_ui.sizer_lbox.add_window_int(
-            Some(&lbox),
-            1,
-            wx::GROW | wx::ALL,
+        let spinctrl = wx::SpinCtrl::builder(Some(&self.base))
+            .id(SpinBtnPage::SpinCtrl.into())
+            .value(&format!("{}", val))
+            .style(flags)
+            .min(min)
+            .max(max)
+            .initial(val)
+            .build();
+
+        let spinctrldbl = wx::SpinCtrlDouble::builder(Some(&self.base))
+            .id(SpinBtnPage::SpinCtrlDouble.into())
+            .value(&format!("{}", val))
+            .style(flags)
+            .min(min.into())
+            .max(max.into())
+            .initial(val.into())
+            .inc(0.1)
+            .build();
+
+        // Add spacers, labels and spin controls to the sizer.
+        let sizer_spin = &config_ui.sizer_spin;
+
+        sizer_spin.add_int_int(0, 0, 1, 0, 0, wx::Object::none());
+        sizer_spin.add_window_int(
+            Some(
+                &wx::StaticText::builder(Some(&self.base))
+                    .label("wxSpinButton")
+                    .build(),
+            ),
+            0,
+            wx::ALIGN_CENTRE | wx::ALL,
             5,
             wx::Object::none(),
         );
-        *self.lbox.borrow_mut() = Some(lbox);
+        sizer_spin.add_window_int(
+            Some(&spinbtn),
+            0,
+            wx::ALIGN_CENTRE | wx::ALL,
+            5,
+            wx::Object::none(),
+        );
+        *self.spinbtn.borrow_mut() = Some(spinbtn);
 
-        // relayout the sizer
-        config_ui.sizer_lbox.layout();
+        sizer_spin.add_int_int(0, 0, 1, 0, 0, wx::Object::none());
+        sizer_spin.add_window_int(
+            Some(
+                &wx::StaticText::builder(Some(&self.base))
+                    .label("wxSpinCtrl")
+                    .build(),
+            ),
+            0,
+            wx::ALIGN_CENTRE | wx::ALL,
+            5,
+            wx::Object::none(),
+        );
+        sizer_spin.add_window_int(
+            Some(&spinctrl),
+            0,
+            wx::ALIGN_CENTRE | wx::ALL,
+            5,
+            wx::Object::none(),
+        );
+        *self.spinctrl.borrow_mut() = Some(spinctrl);
+
+        sizer_spin.add_int_int(0, 0, 1, 0, 0, wx::Object::none());
+        sizer_spin.add_window_int(
+            Some(
+                &wx::StaticText::builder(Some(&self.base))
+                    .label("wxSpinCtrlDouble")
+                    .build(),
+            ),
+            0,
+            wx::ALIGN_CENTRE | wx::ALL,
+            5,
+            wx::Object::none(),
+        );
+        sizer_spin.add_window_int(
+            Some(&spinctrldbl),
+            0,
+            wx::ALIGN_CENTRE | wx::ALL,
+            5,
+            wx::Object::none(),
+        );
+        *self.spinctrldbl.borrow_mut() = Some(spinctrldbl);
+
+        sizer_spin.add_int_int(0, 0, 1, 0, 0, wx::Object::none());
+
+        config_ui.sizer_spin.layout();
+    }
+
+    // is this spinbtn value in range?
+    fn is_valid_value(&self, val: c_int) -> bool {
+        let min = *self.min.borrow();
+        let max = *self.max.borrow();
+        return (val >= min) && (val <= max);
     }
 
     // ----------------------------------------------------------------------------
@@ -221,10 +449,10 @@ impl SpinButtonWidgetsPage {
     fn on_button_reset(&self, config_ui: &ConfigUI) {
         self.reset(config_ui);
 
-        self.create_lbox(config_ui);
+        self.create_spin(config_ui);
     }
 
     fn on_check_box(&self, config_ui: &ConfigUI) {
-        self.create_lbox(config_ui);
+        self.create_spin(config_ui);
     }
 }
