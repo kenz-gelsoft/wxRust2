cebfbd34720e2c9f238dac2f1a92671a08733a9f
KENZ <KENZ.gelsoft@gmail.com>
2022-08-13 08:19:39 +0900

    List core library classe filenames

--- list_classes.py
+++ list_classes.py
@@ -2,7 +2,6 @@ from doxybindgen.model import Class, ClassManager
 from doxybindgen.binding import CxxClassBinding, RustClassBinding
 
 import os
-import subprocess
 import toml
 
 
@@ -13,12 +12,13 @@ def main():
     
     classes = ClassManager()
     parsed = []
-    for wxml in wxml_files():
-        for cls in Class.in_xml(classes, wxml, config['types']):
+    for (file, path) in wxml_files():
+        for cls in Class.in_xml(classes, path, config['types']):
+            cls.file = file
             parsed.append(cls)
     classes.register(parsed)
     
-    print_classes_in_lib(classes, config, 'base')
+    # print_classes_in_lib(classes, config, 'base')
     print_classes_in_lib(classes, config, 'core')
 
 def wxml_files():
@@ -26,16 +26,16 @@ def wxml_files():
         for file in files:
             if (file.startswith('classwx_') and
                 file.endswith('.xml')):
-                yield os.path.join(root, file)
+                yield (file, os.path.join(root, file))
 
 generated = []
 def print_classes_in_lib(classes, config, libname):
-    print('%s:' % (libname,))
+    # print('%s:' % (libname,))
     n = 0
-    for cls in classes.in_lib(libname, generated):
-        print('\t%s' % (cls.name,))
+    for file in sorted(cls.file for cls in classes.in_lib(libname, generated)):
+        print("    'wxml/%s'," % (file,))
         n += 1
-    print('\t%s classes.' % (n))
+    # print('\t%s classes.' % (n))
 
 if __name__ == '__main__':
     main()
