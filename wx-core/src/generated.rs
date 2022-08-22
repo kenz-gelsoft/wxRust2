1607e0a5e7762d3a125df239bc06f121fa79fd22
KENZ <KENZ.gelsoft@gmail.com>
2022-08-23 01:16:57 +0900

    Allow unused import on generated code and tweak macro codegen

--- wx-core/src/generated.rs
+++ wx-core/src/generated.rs
@@ -1,3 +1,6 @@
+#![allow(non_upper_case_globals)]
+#![allow(unused_imports)]
+
 use std::os::raw::{c_double, c_int, c_long, c_uchar, c_uint, c_void};
 
 use super::*;
--- wx-core/src/generated/class_b.rs
+++ wx-core/src/generated/class_b.rs
@@ -1,5 +1,3 @@
-#![allow(non_upper_case_globals)]
-
 use super::*;
 
 // wxBannerWindow
