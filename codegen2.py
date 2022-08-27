e64efaa84326cf979dcfd21d92659917cdf7906e
KENZ <KENZ.gelsoft@gmail.com>
2022-08-28 07:56:51 +0900

    Remove redundant code

--- codegen2.py
+++ codegen2.py
@@ -150,13 +150,7 @@ extern "C" {
 
 def class_rs(initials, libname):
     yield '''\
-#![allow(non_upper_case_globals)]
-#![allow(unused_imports)]
-
-use std::os::raw::{c_double, c_int, c_long, c_uchar, c_uint, c_void};
-
 use super::*;
-use methods::*;
 '''
     for i in initials:
         yield 'pub use class_%s::*;' % (i,)
--- wx-base/src/generated/class.rs
+++ wx-base/src/generated/class.rs
@@ -1,10 +1,4 @@
-#![allow(non_upper_case_globals)]
-#![allow(unused_imports)]
-
-use std::os::raw::{c_double, c_int, c_long, c_uchar, c_uint, c_void};
-
 use super::*;
-use methods::*;
 
 pub use class_c::*;
 pub use class_d::*;
