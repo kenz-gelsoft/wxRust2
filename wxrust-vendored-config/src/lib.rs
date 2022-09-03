fe7f3f6dd7b4d1d66707d1c678d6011c684dc9f1
KENZ <KENZ.gelsoft@gmail.com>
2022-09-03 14:55:24 +0900

    WIP: try moving deps on non-crates.io crates to sample crates

--- wxrust-vendored-config/src/lib.rs
+++ wxrust-vendored-config/src/lib.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::process::Command;
 
 pub fn wx_config_cflags(cc_build: &mut cc::Build) -> &mut cc::Build {
     // from `wx-config --cflags`
@@ -60,59 +59,17 @@ fn dep_links() -> String {
 }
 
 fn wx_config(args: &[&str]) -> String {
-    if cfg!(feature = "vendored") {
-        let flags: Vec<_> = env::var(format!("DEP_WX_{}_CFLAGS", dep_links()))
-            .unwrap()
-            .split_whitespace()
-            .map(ToOwned::to_owned)
-            .collect();
-        let (ldflags, cflags): (Vec<_>, Vec<_>) = flags
-            .into_iter()
-            .partition(|f| f.starts_with("-l") || f.starts_with("-L"));
-        return if args.contains(&"--cflags") {
-            cflags.join(" ")
-        } else {
-            ldflags.join(" ")
-        };
-    }
-
-    if cfg!(windows) {
-        wx_config_win(args)
-    } else {
-        let output = Command::new("wx-config")
-            .args(args)
-            .output()
-            .expect("failed execute wx-config command.");
-        String::from_utf8_lossy(&output.stdout).to_string()
-    }
-}
-
-fn wx_config_win(args: &[&str]) -> String {
-    let wxwin = env::var("wxwin")
-        .expect("Set 'wxwin' environment variable to point the wxMSW binaries dir.");
-    // TODO: support linking with the wx debug DLL
-    let is_debug = false; //env::var("PROFILE").unwrap() == "debug";
-    let d_or_not = if is_debug { "d" } else { "" };
-    if args.contains(&"--cflags") {
-        let mut cflags = vec![
-            format!("-I{}\\include", wxwin),
-            format!("-I{}\\lib\\vc14x_x64_dll\\mswu{}", wxwin, d_or_not),
-            "-DWXUSINGDLL".to_string(),
-        ];
-        if is_debug {
-            cflags.push("-D_DEBUG".to_string());
-        } else {
-            cflags.push("-D__NO_VC_CRTDBG__".to_string());
-            cflags.push("-DwxDEBUG_LEVEL=0".to_string());
-            cflags.push("-DNDEBUG".to_string());
-        }
+    let flags: Vec<_> = env::var(format!("DEP_WX_{}_CFLAGS", dep_links()))
+        .unwrap()
+        .split_whitespace()
+        .map(ToOwned::to_owned)
+        .collect();
+    let (ldflags, cflags): (Vec<_>, Vec<_>) = flags
+        .into_iter()
+        .partition(|f| f.starts_with("-l") || f.starts_with("-L"));
+    return if args.contains(&"--cflags") {
         cflags.join(" ")
     } else {
-        let libs = vec![
-            format!("-L{}\\lib\\vc14x_x64_dll", wxwin),
-            format!("-lwxbase32u{}", d_or_not),
-            format!("-lwxmsw32u{}_core", d_or_not),
-        ];
-        libs.join(" ")
-    }
+        ldflags.join(" ")
+    };
 }
